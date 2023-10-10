63977a4857928f9686a5aefe4969078e482dfdee
Vaibhav Gupta <vbv@gloo.chat>
2023-10-10 00:24:00 -0700

    [BUMP:cli:0.4.0-canary.0] [BUMP:py_client:1.3.0+canary.0] [BUMP:vscode_ext:0.5.0-canary.0]

--- clients/python/gloo_py/__init__.py
+++ clients/python/gloo_py/__init__.py
@@ -7,7 +7,7 @@ from gloo_internal.tracer import trace, update_trace_tags
 from gloo_internal.llm_client import LLMClient, OpenAILLMClient
 
 
-__version__ = "1.2.0+canary.0"
+__version__ = "1.3.0+canary.0"
 
 __all__ = [
     "CodeVariant",
