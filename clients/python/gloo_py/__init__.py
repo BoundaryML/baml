c77fe1608c2b805bacbfbbc748b597d8f0d9acbb
Vaibhav Gupta <vbv@gloo.chat>
2023-10-15 19:02:11 -0700

    [BUMP:py_client:1.3.0.dev5]

--- clients/python/gloo_py/__init__.py
+++ clients/python/gloo_py/__init__.py
@@ -7,7 +7,7 @@ from gloo_internal.tracer import trace, update_trace_tags
 from gloo_internal.llm_client import LLMClient, OpenAILLMClient
 
 
-__version__ = "1.3.0.dev4"
+__version__ = "1.3.0.dev5"
 
 __all__ = [
     "CodeVariant",
