b4cc810f5944dfa80a620618ab34e194418bfd32
Vaibhav Gupta <vbv@gloo.chat>
2023-10-24 14:50:48 -0700

    Valdiate the function name is set

--- engine/baml/baml-core/src/validate/validation_pipeline/validations/variants.rs
+++ engine/baml/baml-core/src/validate/validation_pipeline/validations/variants.rs
@@ -5,6 +5,13 @@ use crate::{ast::WithSpan, validate::validation_pipeline::context::Context};
 
 pub(super) fn validate(ctx: &mut Context<'_>) {
     for variant in ctx.db.walk_variants() {
+        if variant.walk_function().is_none() {
+            ctx.push_error(DatamodelError::new_validation_error(
+                &format!("Function not found: {}", variant.function_name()),
+                variant.ast_variant().span().clone(),
+            ));
+        }
+
         let client = &variant.properties().client;
 
         if ctx.db.find_client(client).is_none() {
