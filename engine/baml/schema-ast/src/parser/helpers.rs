163bdb9a3806281e428e61b51a38123942bdc27a
Vaibhav Gupta <vbv@gloo.chat>
2023-11-01 18:13:15 -0700

    Update pest to accept block style grammars

--- engine/baml/schema-ast/src/parser/helpers.rs
+++ engine/baml/schema-ast/src/parser/helpers.rs
@@ -5,7 +5,7 @@ pub type Pair<'a> = pest::iterators::Pair<'a, Rule>;
 #[track_caller]
 pub fn parsing_catch_all(token: &Pair<'_>, kind: &str) {
     match token.as_rule() {
-        Rule::empty_lines | Rule::trailing_comment | Rule::comment_block => {}
+        Rule::empty_lines | Rule::trailing_comment | Rule::comment_block | Rule::block_comment => {}
         x => unreachable!(
             "Encountered impossible {} during parsing: {:?} {:?}",
             kind,
