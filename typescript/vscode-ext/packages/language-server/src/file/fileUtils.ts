6ee3a4ffde62af38d3a2a2d7c8b8f986aa927118
Vaibhav Gupta <vbv@gloo.chat>
2023-12-12 16:41:38 -0800

    Adding tripple /

--- typescript/vscode-ext/packages/language-server/src/file/fileUtils.ts
+++ typescript/vscode-ext/packages/language-server/src/file/fileUtils.ts
@@ -1,7 +1,7 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import { TextDocument } from 'vscode-languageserver-textdocument';
-import { URI } from 'vscode-uri';
+import * as path from 'path'
+import * as fs from 'fs'
+import { TextDocument } from 'vscode-languageserver-textdocument'
+import { URI } from 'vscode-uri'
 
 // export function findTopLevelParent(filePath: string) {
 //   let currentPath = filePath;
@@ -15,52 +15,44 @@ import { URI } from 'vscode-uri';
 //     }
 //   }
 
-
 //   if (parentDir !== null) {
 //     return parentDir;
 //   }
 //   return null;
 // }
 
-
 export function gatherFiles(dir: string, debug: boolean = false, fileList: string[] = []): string[] {
-  let uri = URI.parse(dir);
-  let dirPath = uri.fsPath;
-  const files = fs.readdirSync(dirPath);
+  let uri = URI.parse(dir)
+  let dirPath = uri.fsPath
+  const files = fs.readdirSync(dirPath)
   if (debug) {
-    console.log(`Gathering files from ${dirPath}. uri: ${uri.toString()}`);
-    console.log("files " + JSON.stringify(files, null, 2));
+    console.log(`Gathering files from ${dirPath}. uri: ${uri.toString()}`)
+    console.log('files ' + JSON.stringify(files, null, 2))
   }
 
   files.forEach((file) => {
-    const filePath = path.join(dirPath, file);
-    const fileStat = fs.statSync(filePath);
+    const filePath = path.join(dirPath, file)
+    const fileStat = fs.statSync(filePath)
     if (debug) {
-      console.log(`Checking ${filePath}`);
-      console.log(`isDirectory: ${fileStat.isDirectory()}`);
+      console.log(`Checking ${filePath}`)
+      console.log(`isDirectory: ${fileStat.isDirectory()}`)
     }
 
     if (fileStat.isDirectory()) {
-      gatherFiles(`file://${filePath}`, debug, fileList);
+      gatherFiles(`file:///${filePath}`, debug, fileList)
     } else {
       // check if it has .baml extension
       if (filePath.endsWith('.baml') || filePath.endsWith('.json')) {
-        fileList.push(filePath);
+        fileList.push(filePath)
       }
     }
-  });
+  })
 
-  return fileList.map((filePath) => URI.file(filePath).toString());
+  return fileList.map((filePath) => URI.file(filePath).toString())
 }
 
-
 export function convertToTextDocument(filePath: string): TextDocument {
-  const fileContent = fs.readFileSync(URI.parse(filePath).fsPath, 'utf-8');
-  const fileExtension = path.extname(filePath);
-  return TextDocument.create(
-    filePath.toString(),
-    fileExtension === '.baml' ? 'baml' : 'json',
-    1,
-    fileContent
-  );
+  const fileContent = fs.readFileSync(URI.parse(filePath).fsPath, 'utf-8')
+  const fileExtension = path.extname(filePath)
+  return TextDocument.create(filePath.toString(), fileExtension === '.baml' ? 'baml' : 'json', 1, fileContent)
 }
