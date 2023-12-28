0e36341a5bcbb9177eaec0d5db10d9df6d7e79ab
Sam Lijin <dev@sxlijin.com>
2023-12-27 17:12:26 -0700

    rename to repr

--- engine/baml-lib/baml-core/src/generate/lockfile/repr.rs
+++ engine/baml-lib/baml-core/src/generate/lockfile/repr.rs
@@ -4,8 +4,8 @@ use internal_baml_parser_database::walkers::{
 use internal_baml_schema_ast::ast::{self, FieldType, Identifier, TypeValue, WithName};
 use serde_json::{json, Value};
 
-pub(crate) trait WithJsonSchema {
-    fn json_schema(&self) -> Value;
+pub(crate) trait WithRepr {
+    fn repr(&self) -> Value;
 }
 
 #[derive(serde::Serialize)]
@@ -27,8 +27,8 @@ struct Enum {
     values: Vec<String>,
 }
 
-impl WithJsonSchema for EnumWalker<'_> {
-    fn json_schema(&self) -> Value {
+impl WithRepr for EnumWalker<'_> {
+    fn repr(&self) -> Value {
         serde_json::to_value(Enum {
             name: self.name().to_string(),
             values: self.values().map(|v| v.name().to_string()).collect(),
@@ -49,8 +49,8 @@ struct Class {
     properties: Vec<Field>,
 }
 
-impl WithJsonSchema for ClassWalker<'_> {
-    fn json_schema(&self) -> Value {
+impl WithRepr for ClassWalker<'_> {
+    fn repr(&self) -> Value {
         serde_json::to_value(Class {
             name: self.name().to_string(),
             properties: self
@@ -67,6 +67,42 @@ impl WithJsonSchema for ClassWalker<'_> {
 }
 
 #[derive(serde::Serialize)]
+enum ImplementationType {
+    LLM,
+}
+
+#[derive(serde::Serialize)]
+struct Implementation {
+    r#type: ImplementationType,
+    name: String,
+
+    prompt: String,
+    input_replacers: Vec<String>,
+    output_replacers: Vec<String>,
+    client: String,
+    //        //            "type": "llm",
+    //        //            "name": StringSpan::new(i.ast_variant().name(), &i.identifier().span()),
+    //        //            "prompt_key": {
+    //        //                "start": props.prompt.key_span.start,
+    //        //                "end": props.prompt.key_span.end,
+    //        //                "source_file": props.prompt.key_span.file.path(),
+    //        //            },
+    //        //            "prompt": props.prompt.value,
+    //        //            "input_replacers": props.replacers.0.iter().map(
+    //        //                |r| json!({
+    //        //                    "key": r.0.key(),
+    //        //                    "value": r.1,
+    //        //                })
+    //        //            ).collect::<Vec<_>>(),
+    //        //            "output_replacers": props.replacers.1.iter().map(
+    //        //                |r| json!({
+    //        //                    "key": r.0.key(),
+    //        //                    "value": r.1,
+    //        //                })
+    //        //            ).collect::<Vec<_>>(),
+    //        //            "client": schema.db.find_client(&props.client.value).map(|c| StringSpan::new(c.name(), &c.identifier().span())).unwrap_or_else(|| StringSpan::new(&props.client.value, &props.client.span)),
+}
+#[derive(serde::Serialize)]
 struct Function {
     name: String,
     // DO NOT LAND - need clarification
@@ -78,8 +114,8 @@ struct Function {
     impls: Vec<Implementation>,
 }
 
-impl WithJsonSchema for FunctionWalker<'_> {
-    fn json_schema(&self) -> Value {
+impl WithRepr for FunctionWalker<'_> {
+    fn repr(&self) -> Value {
         serde_json::to_value(Function {
             name: self.name().to_string(),
             named_inputs: if let ast::FunctionArgs::Named(arg_list) = self.ast_function().input() {
@@ -97,7 +133,17 @@ impl WithJsonSchema for FunctionWalker<'_> {
                 ast::FunctionArgs::Named(arg_list) => Type::PRIMITIVE(Primitive::STRING),
                 ast::FunctionArgs::Unnamed(arg) => Type::PRIMITIVE(Primitive::STRING),
             },
-            impls: vec![],
+            impls: self
+                .walk_variants()
+                .map(|e| Implementation {
+                    r#type: ImplementationType::LLM,
+                    name: e.name().to_string(),
+                    prompt: e.properties().prompt.value.clone(),
+                    input_replacers: vec![],
+                    output_replacers: vec![],
+                    client: e.properties().client.value.clone(),
+                })
+                .collect(),
         })
         .unwrap()
         //    json!({
@@ -179,99 +225,62 @@ impl WithJsonSchema for FunctionWalker<'_> {
     }
 }
 
-impl WithJsonSchema for FieldType {
-    fn json_schema(&self) -> Value {
-        match self {
-            FieldType::Identifier(_, idn) => match idn {
-                Identifier::Primitive(t, ..) => json!({
-                    "type": match t {
-                        TypeValue::String => "string",
-                        TypeValue::Int => "integer",
-                        TypeValue::Float => "number",
-                        TypeValue::Bool => "boolean",
-                        TypeValue::Null => "undefined",
-                        TypeValue::Char => "string",
-                    }
-                }),
-                Identifier::Local(name, _) => json!({
-                    "$ref": format!("#/definitions/{}", name),
-                }),
-                _ => panic!("Not implemented"),
-            },
-            FieldType::List(item, dims, _) => {
-                let mut inner = json!({
-                    "type": "array",
-                    "items": (*item).json_schema()
-                });
-                for _ in 1..*dims {
-                    inner = json!({
-                        "type": "array",
-                        "items": inner
-                    });
-                }
-
-                return inner;
-            }
-            FieldType::Dictionary(kv, _) => json!({
-                "type": "object",
-                "additionalProperties": {
-                    "type": (*kv).1.json_schema(),
-                }
-            }),
-            FieldType::Union(_, t, _) => json!({
-                "anyOf": t.iter().map(|t| {
-                    let res = t.json_schema();
-                    // if res is a map, add a "title" field
-                    if let Value::Object(res) = &res {
-                        let mut res = res.clone();
-                        res.insert("title".to_string(), json!(t.to_string()));
-                        return json!(res);
-                    }
-                    res
-                }
-            ).collect::<Vec<_>>(),
-            }),
-            FieldType::Tuple(_, t, _) => json!({
-                "type": "array",
-                "items": t.iter().map(|t| t.json_schema()).collect::<Vec<_>>(),
-            }),
-        }
-    }
-}
-
-#[derive(serde::Serialize)]
-enum ImplementationType {
-    LLM,
-}
-
-#[derive(serde::Serialize)]
-struct Implementation {
-    r#type: ImplementationType,
-    name: String,
-
-    prompt: String,
-    input_replacers: Vec<String>,
-    output_replacers: Vec<String>,
-    client: String,
-    //        //            "type": "llm",
-    //        //            "name": StringSpan::new(i.ast_variant().name(), &i.identifier().span()),
-    //        //            "prompt_key": {
-    //        //                "start": props.prompt.key_span.start,
-    //        //                "end": props.prompt.key_span.end,
-    //        //                "source_file": props.prompt.key_span.file.path(),
-    //        //            },
-    //        //            "prompt": props.prompt.value,
-    //        //            "input_replacers": props.replacers.0.iter().map(
-    //        //                |r| json!({
-    //        //                    "key": r.0.key(),
-    //        //                    "value": r.1,
-    //        //                })
-    //        //            ).collect::<Vec<_>>(),
-    //        //            "output_replacers": props.replacers.1.iter().map(
-    //        //                |r| json!({
-    //        //                    "key": r.0.key(),
-    //        //                    "value": r.1,
-    //        //                })
-    //        //            ).collect::<Vec<_>>(),
-    //        //            "client": schema.db.find_client(&props.client.value).map(|c| StringSpan::new(c.name(), &c.identifier().span())).unwrap_or_else(|| StringSpan::new(&props.client.value, &props.client.span)),
-}
+// impl WithJsonSchema for FieldType {
+//     fn json_schema(&self) -> Value {
+//         match self {
+//             FieldType::Identifier(_, idn) => match idn {
+//                 Identifier::Primitive(t, ..) => json!({
+//                     "type": match t {
+//                         TypeValue::String => "string",
+//                         TypeValue::Int => "integer",
+//                         TypeValue::Float => "number",
+//                         TypeValue::Bool => "boolean",
+//                         TypeValue::Null => "undefined",
+//                         TypeValue::Char => "string",
+//                     }
+//                 }),
+//                 Identifier::Local(name, _) => json!({
+//                     "$ref": format!("#/definitions/{}", name),
+//                 }),
+//                 _ => panic!("Not implemented"),
+//             },
+//             FieldType::List(item, dims, _) => {
+//                 let mut inner = json!({
+//                     "type": "array",
+//                     "items": (*item).json_schema()
+//                 });
+//                 for _ in 1..*dims {
+//                     inner = json!({
+//                         "type": "array",
+//                         "items": inner
+//                     });
+//                 }
+//
+//                 return inner;
+//             }
+//             FieldType::Dictionary(kv, _) => json!({
+//                 "type": "object",
+//                 "additionalProperties": {
+//                     "type": (*kv).1.json_schema(),
+//                 }
+//             }),
+//             FieldType::Union(_, t, _) => json!({
+//                 "anyOf": t.iter().map(|t| {
+//                     let res = t.json_schema();
+//                     // if res is a map, add a "title" field
+//                     if let Value::Object(res) = &res {
+//                         let mut res = res.clone();
+//                         res.insert("title".to_string(), json!(t.to_string()));
+//                         return json!(res);
+//                     }
+//                     res
+//                 }
+//             ).collect::<Vec<_>>(),
+//             }),
+//             FieldType::Tuple(_, t, _) => json!({
+//                 "type": "array",
+//                 "items": t.iter().map(|t| t.json_schema()).collect::<Vec<_>>(),
+//             }),
+//         }
+//     }
+// }
