7af0944973b71afdc084d193b89de37d6eb47c0f
hellovai <vbv@gloo.chat>
2023-12-29 23:41:58 -0800

    Fix test case issue with including " and \n and other special characters. (#229)
    Currently this is still quite hacky, we should do something more robust
    and actually validate the json at compile time in BAML rather than what
    we are doing today which is evaluating it at runtime.
    
    Tested with single and multi parameter (string and non-string values)

--- engine/baml-lib/baml-core/src/generate/generate_python_client/template.rs
+++ engine/baml-lib/baml-core/src/generate/generate_python_client/template.rs
@@ -130,7 +130,7 @@ fn use_partial(
         }
         HSTemplate::MultiArgTestSnippet => {
             register_partial_file!(reg, "tests", "multi_arg_snippet");
-            f.add_import("json", "loads");
+            f.add_import("json", "dumps");
             f.add_import("..__do_not_import.generated_baml_client", "baml");
             f.add_import("baml_lib._impl.deserializer", "Deserializer");
             String::from("multi_arg_snippet")
--- engine/baml-lib/baml-core/src/generate/generate_python_client/templates/tests/single_arg_snippet.hb
+++ engine/baml-lib/baml-core/src/generate/generate_python_client/templates/tests/single_arg_snippet.hb
@@ -1,9 +1,8 @@
 @baml.{{function_name}}.test
 async def test_{{test_case_name}}({{function_name}}Impl: I{{function_name}}):
+    content = dumps({{{test_case_input}}})
     deserializer = Deserializer[{{test_case_type}}]({{test_case_type}}) # type: ignore
-    param = deserializer.from_string("""\
-{{{test_case_input}}}\
-""")
+    param = deserializer.from_string(content)
     await {{function_name}}Impl(param)
 
 
