7220048f92fc420064a3d1660cfff0b18011ba49
aaronvg <aaron@boundaryml.com>
2024-01-18 21:44:22 -0800

    Support python 3.8, fix url not showing in pytest, remove old files (#344)

--- clients/python/gloo_py/testing/__init__.py
+++
@@ -1,3 +0,0 @@
-from gloo_testing import gloo_test
-
-__all__ = ["gloo_test"]
--- clients/python/gloo_stringify/errors.py
+++
@@ -1,5 +0,0 @@
-class StringifyError(Exception):
-    """Raised when an error occurs while stringifying an object."""
-
-    def __init__(self, message: str) -> None:
-        super().__init__(message)
