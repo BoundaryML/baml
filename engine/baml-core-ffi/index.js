9a72f372f9028c0102fdc115c6f29fe1e3c21e60
Aaron Villalpando <aaron@boundaryml.com>
2024-03-06 13:53:34 -0800

    rename

--- engine/baml-core-ffi/index.js
+++ engine/baml-core-ffi/index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'baml-core-napi.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'baml-core.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./baml-core-napi.android-arm64.node')
+            nativeBinding = require('./baml-core.android-arm64.node')
           } else {
-            nativeBinding = require('@boundaryml/baml-core-napi-android-arm64')
+            nativeBinding = require('@boundaryml/baml-core-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'baml-core-napi.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'baml-core.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./baml-core-napi.android-arm-eabi.node')
+            nativeBinding = require('./baml-core.android-arm-eabi.node')
           } else {
-            nativeBinding = require('@boundaryml/baml-core-napi-android-arm-eabi')
+            nativeBinding = require('@boundaryml/baml-core-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -62,36 +62,36 @@ switch (platform) {
   case 'win32':
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'baml-core-napi.win32-x64-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'baml-core.win32-x64-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./baml-core-napi.win32-x64-msvc.node')
+            nativeBinding = require('./baml-core.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('@boundaryml/baml-core-napi-win32-x64-msvc')
+            nativeBinding = require('@boundaryml/baml-core-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'ia32':
-        localFileExisted = existsSync(join(__dirname, 'baml-core-napi.win32-ia32-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'baml-core.win32-ia32-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./baml-core-napi.win32-ia32-msvc.node')
+            nativeBinding = require('./baml-core.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('@boundaryml/baml-core-napi-win32-ia32-msvc')
+            nativeBinding = require('@boundaryml/baml-core-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'baml-core-napi.win32-arm64-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'baml-core.win32-arm64-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./baml-core-napi.win32-arm64-msvc.node')
+            nativeBinding = require('./baml-core.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('@boundaryml/baml-core-napi-win32-arm64-msvc')
+            nativeBinding = require('@boundaryml/baml-core-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -102,35 +102,35 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'baml-core-napi.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'baml-core.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./baml-core-napi.darwin-universal.node')
+        nativeBinding = require('./baml-core.darwin-universal.node')
       } else {
-        nativeBinding = require('@boundaryml/baml-core-napi-darwin-universal')
+        nativeBinding = require('@boundaryml/baml-core-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'baml-core-napi.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'baml-core.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./baml-core-napi.darwin-x64.node')
+            nativeBinding = require('./baml-core.darwin-x64.node')
           } else {
-            nativeBinding = require('@boundaryml/baml-core-napi-darwin-x64')
+            nativeBinding = require('@boundaryml/baml-core-darwin-x64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'baml-core-napi.darwin-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'baml-core.darwin-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./baml-core-napi.darwin-arm64.node')
+            nativeBinding = require('./baml-core.darwin-arm64.node')
           } else {
-            nativeBinding = require('@boundaryml/baml-core-napi-darwin-arm64')
+            nativeBinding = require('@boundaryml/baml-core-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -144,12 +144,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'baml-core-napi.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'baml-core.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./baml-core-napi.freebsd-x64.node')
+        nativeBinding = require('./baml-core.freebsd-x64.node')
       } else {
-        nativeBinding = require('@boundaryml/baml-core-napi-freebsd-x64')
+        nativeBinding = require('@boundaryml/baml-core-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -159,23 +159,23 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, 'baml-core-napi.linux-x64-musl.node'))
+          localFileExisted = existsSync(join(__dirname, 'baml-core.linux-x64-musl.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./baml-core-napi.linux-x64-musl.node')
+              nativeBinding = require('./baml-core.linux-x64-musl.node')
             } else {
-              nativeBinding = require('@boundaryml/baml-core-napi-linux-x64-musl')
+              nativeBinding = require('@boundaryml/baml-core-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, 'baml-core-napi.linux-x64-gnu.node'))
+          localFileExisted = existsSync(join(__dirname, 'baml-core.linux-x64-gnu.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./baml-core-napi.linux-x64-gnu.node')
+              nativeBinding = require('./baml-core.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('@boundaryml/baml-core-napi-linux-x64-gnu')
+              nativeBinding = require('@boundaryml/baml-core-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -184,23 +184,23 @@ switch (platform) {
         break
       case 'arm64':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, 'baml-core-napi.linux-arm64-musl.node'))
+          localFileExisted = existsSync(join(__dirname, 'baml-core.linux-arm64-musl.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./baml-core-napi.linux-arm64-musl.node')
+              nativeBinding = require('./baml-core.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('@boundaryml/baml-core-napi-linux-arm64-musl')
+              nativeBinding = require('@boundaryml/baml-core-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, 'baml-core-napi.linux-arm64-gnu.node'))
+          localFileExisted = existsSync(join(__dirname, 'baml-core.linux-arm64-gnu.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./baml-core-napi.linux-arm64-gnu.node')
+              nativeBinding = require('./baml-core.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('@boundaryml/baml-core-napi-linux-arm64-gnu')
+              nativeBinding = require('@boundaryml/baml-core-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -208,12 +208,12 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'baml-core-napi.linux-arm-gnueabihf.node'))
+        localFileExisted = existsSync(join(__dirname, 'baml-core.linux-arm-gnueabihf.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./baml-core-napi.linux-arm-gnueabihf.node')
+            nativeBinding = require('./baml-core.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('@boundaryml/baml-core-napi-linux-arm-gnueabihf')
+            nativeBinding = require('@boundaryml/baml-core-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
