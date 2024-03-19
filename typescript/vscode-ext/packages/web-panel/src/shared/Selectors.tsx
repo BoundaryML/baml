9b240e960c6c8bf8909059f533a9580b8acd0daf
Vaibhav Gupta <vbv@boundaryml.com>
2024-03-19 10:06:41 -0700

    Dropdown for function selector

--- typescript/vscode-ext/packages/web-panel/src/shared/Selectors.tsx
+++ typescript/vscode-ext/packages/web-panel/src/shared/Selectors.tsx
@@ -1,28 +1,64 @@
 import { VSCodeDropdown, VSCodeLink, VSCodeOption } from '@vscode/webview-ui-toolkit/react'
 import { useSelections } from './hooks'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { ASTContext } from './ASTProvider'
 import { vscode } from '@/utils/vscode'
 import Link from './Link'
 import TypeComponent from './TypeComponent'
 import { ProjectToggle } from './ProjectPanel'
+import { Command, CommandInput, CommandEmpty, CommandGroup, CommandItem, CommandList } from '@/components/ui/command'
+import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { ChevronsUpDown, Check } from 'lucide-react'
+import SearchBarWithSelector from '@/lib/searchbar'
 
-export const FunctionSelector: React.FC = () => {
+const FunctionDropdown: React.FC = () => {
+  const [open, setOpen] = useState(false)
   const {
-    projects,
-    selectedProjectId,
     db: { functions },
     setSelection,
   } = useContext(ASTContext)
+
   const { func } = useSelections()
-  const function_names = functions.map((func) => func.name.value)
+  const value = func?.name.value;
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant='ghost'
+          aria-expanded={open}
+          className="w-[250px] sm:w-[450px] justify-between flex hover:bg-primary/90 hover:text-foreground"
+        >
+          <span className="min-w-[200px] sm:min-w-[400px] truncate w-full text-left">{value ?? "Select a function..."}</span>
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-1/3 min-w-[400px] p-0">
+        <SearchBarWithSelector
+          options={functions.map((func) => ({ value: func.name.value }))}
+          onChange={(value) => {
+            setSelection(undefined, value, undefined, undefined, undefined)
+            setOpen(false)
+          }}
+        />
+      </PopoverContent>
+    </Popover>
+  )
+
+}
+
+export const FunctionSelector: React.FC = () => {
+  const { func } = useSelections()
 
   return (
     <div className="flex flex-col items-start gap-1">
       <div className="flex flex-row items-center gap-1">
         <ProjectToggle />
 
-        <span className="font-light">Function</span>
+        <FunctionDropdown />
+        {/* <span className="font-light">Function</span>
         <VSCodeDropdown
           value={func?.name.value ?? '<not-picked>'}
           onChange={(event) =>
@@ -40,11 +76,12 @@ export const FunctionSelector: React.FC = () => {
               {func}
             </VSCodeOption>
           ))}
-        </VSCodeDropdown>
+        </VSCodeDropdown> */}
       </div>
       {func && (
         <div className="flex flex-row items-center gap-0 text-xs">
-          <Link item={func.name} />(
+          <span className='text-muted-foreground pr-1'>Fn signature</span>
+          (
           {func.input.arg_type === 'positional' ? (
             <div className="flex flex-row gap-1">
               arg: <TypeComponent typeString={func.input.type} />
@@ -58,7 +95,7 @@ export const FunctionSelector: React.FC = () => {
               ))}
             </div>
           )}
-          ) {'->'} {func.output.arg_type === 'positional' && <TypeComponent typeString={func.output.type} />}
+          ) {'→'} {func.output.arg_type === 'positional' && <TypeComponent typeString={func.output.type} />}
         </div>
       )}
     </div>
