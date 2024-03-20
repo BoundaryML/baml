465450e9e79c68cb437db3eede68044d13f80875
Aaron Villalpando <aaron@boundaryml.com>
2024-03-20 13:12:33 -0700

    Fix ts client bundle export paths

--- clients/ts/tsup.config.ts
+++ clients/ts/tsup.config.ts
@@ -5,8 +5,14 @@ const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts",
+    "src/deserializer/deserializer.ts",
+    "src/client_manager/index.ts",
+    "src/ffi_layer.ts",
+    "src/baml_test_runner/index.ts",
+    "src/deserializer/diagnostics.ts"],
   format: ["cjs", "esm"],
   minify: false,
-  sourcemap: true,
+  sourcemap: false,
+  // external: ["jest-resolve/build/default_resolver"]
 });
