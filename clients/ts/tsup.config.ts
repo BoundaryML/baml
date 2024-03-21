800670e8c3310851837c29e52219a646152fb8b8
aaronvg <aaron@boundaryml.com>
2024-03-21 10:15:25 -0700

    Fix ts client bundle export paths (#490)

--- clients/ts/tsup.config.ts
+++ clients/ts/tsup.config.ts
@@ -5,8 +5,11 @@ const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   clean: true,
   dts: true,
+  // we can't split them into chunks yet cause the ffi_layer.ts gets initialized in each dependent chunk and messes up the tracing state. Until that is fixed we just bundle it up into one file, and set fake export paths in the package.json pretending we have many different directories.
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   minify: false,
-  sourcemap: true,
+  sourcemap: false,
+  splitting: false,
+  // cjsInterop: true,
 });
