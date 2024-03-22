681d5aace2649437cc10cc47780b45b05a0a4fa7
Aaron Villalpando <aaron@boundaryml.com>
2024-03-22 16:09:05 -0700

    add some more exports in ts client

--- clients/ts/src/index.ts
+++ clients/ts/src/index.ts
@@ -1,6 +1,8 @@
 import { FireBamlEvent } from "./ffi_layer";
 import * as dotenv from 'dotenv';
 import { BamlTestRunner } from "./baml_test_runner";
+import { DeserializerException, DeserializerError } from "./deserializer/diagnostics";
+import { LLMException } from "./client_manager/errors";
 const setTags = FireBamlEvent.tags;
 
 const loadEnvVars = () => {
@@ -12,6 +14,7 @@ export { setTags, loadEnvVars };
 export { Deserializer, registerEnumDeserializer, registerObjectDeserializer } from "./deserializer/deserializer";
 export { clientManager, ConstantDelayRetryPolicy, ExponentialBackoffRetryPolicy } from "./client_manager";
 export { trace, traceAsync, FireBamlEvent, BamlTracer } from "./ffi_layer";
+export { DeserializerException, DeserializerError, LLMException };
 
 
 export default BamlTestRunner;
