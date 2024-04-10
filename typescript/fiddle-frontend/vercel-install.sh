4679b70ac85fbe86b0d2ae3d52bd6d72a58b5882
Aaron Villalpando <aaron@boundaryml.com>
2024-04-09 18:33:24 -0700

    some more scripts

--- typescript/fiddle-frontend/vercel-install.sh
+++ typescript/fiddle-frontend/vercel-install.sh
@@ -4,6 +4,9 @@ set -e
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 export PATH="/vercel/.cargo/bin:$PATH"
 
+# source $HOME/.cargo/env
+
+which cargo
 cd ../../engine/baml-schema-wasm
 # cargo install
 rustup target add wasm32-unknown-unknown
@@ -12,3 +15,4 @@ cargo install -f wasm-bindgen-cli@0.2.87
 
 cargo build
 cd ../../typescript/fiddle-frontend
+turbo build --filter=@baml/prompt-fiddle-next
