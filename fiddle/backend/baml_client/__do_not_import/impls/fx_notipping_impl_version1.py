f004052294f015da7b3f8ccf8f24f653d764326f
Vaibhav Gupta <vbv@boundaryml.com>
2024-04-15 10:16:02 -0700

    adding linter

--- fiddle/backend/baml_client/__do_not_import/impls/fx_notipping_impl_version1.py
+++ fiddle/backend/baml_client/__do_not_import/impls/fx_notipping_impl_version1.py
@@ -8,9 +8,9 @@
 # fmt: off
 
 from ..clients.client_gpt4turbo import GPT4Turbo
-from ..functions.fx_contradictions import BAMLContradictions
-from ..types.classes.cls_linteroutput2 import LinterOutput2
-from ..types.partial.classes.cls_linteroutput2 import PartialLinterOutput2
+from ..functions.fx_notipping import BAMLNoTipping
+from ..types.classes.cls_linteroutput import LinterOutput
+from ..types.partial.classes.cls_linteroutput import PartialLinterOutput
 from baml_core.provider_manager.llm_response import LLMResponse
 from baml_core.stream import AsyncStream
 from baml_lib._impl.deserializer import Deserializer
@@ -20,30 +20,20 @@ from typing import List
 import typing
 # Impl: version1
 # Client: GPT4Turbo
-# An implementation of Contradictions.
+# An implementation of NoTipping.
 
 __prompt_template = """\
-Find any CONFLICTING / CONTRADICTING words or phrases in-between the <INSTRUCTIONS> tags and output a set of diagnostics matching the output schema.
+Given the INSTRUCTIONS below, check if the text asks for tips. If so, return a diagnostic with the information on the request.
 
-When there is a contradiction or conflicting statement just make an assumption as to what the actual intent is and put that assumption in the "recommendation". The "fix" should incorporate that assumption.
-
-DO NOT match on ambiguous terms. Only match on clear contradictions.
-IGNORE optional fields like (id string?)
-
-Allow advanced prompting techniques that improve the model's results like:
-- Providing examples
-- Chain of thought
-- Tipping for valid json
-
-
+--------------------
 <INSTRUCTIONS>
 {arg}
 </INSTRUCTIONS>
+--------------------
 
-Output JSON format (only include these fields, and no others). Explain your reasoning in 2-3 brief sentences before writing out the json:
+
+Output the diagnostic in this JSON format (only include these fields, and no others):
 {
-  // The conflicting statements or words in the text.
-  "conflictingStatements": string[],
   // Explain why the linting error was raised.
   "reason": string,
   // The phrase that triggered the linter error. Write it EXACTLY as it appears in the PROMPT. If it's more than 10 words, just match the first 10 words.
@@ -66,10 +56,10 @@ __input_replacers = {
 
 # We ignore the type here because baml does some type magic to make this work
 # for inline SpecialForms like Optional, Union, List.
-__deserializer = Deserializer[List[LinterOutput2]](List[LinterOutput2])  # type: ignore
+__deserializer = Deserializer[List[LinterOutput]](List[LinterOutput])  # type: ignore
 
 # Add a deserializer that handles stream responses, which are all Partial types
-__partial_deserializer = Deserializer[List[LinterOutput2]](List[LinterOutput2])  # type: ignore
+__partial_deserializer = Deserializer[List[LinterOutput]](List[LinterOutput])  # type: ignore
 
 
 
@@ -77,17 +67,17 @@ __partial_deserializer = Deserializer[List[LinterOutput2]](List[LinterOutput2]) 
 
 
 
-async def version1(arg: str, /) -> List[LinterOutput2]:
+async def version1(arg: str, /) -> List[LinterOutput]:
     response = await GPT4Turbo.run_prompt_template(template=__prompt_template, replacers=__input_replacers, params=dict(arg=arg))
     deserialized = __deserializer.from_string(response.generated)
     return deserialized
 
 
-def version1_stream(arg: str, /) -> AsyncStream[List[LinterOutput2], List[LinterOutput2]]:
+def version1_stream(arg: str, /) -> AsyncStream[List[LinterOutput], List[LinterOutput]]:
     def run_prompt() -> typing.AsyncIterator[LLMResponse]:
         raw_stream = GPT4Turbo.run_prompt_template_stream(template=__prompt_template, replacers=__input_replacers, params=dict(arg=arg))
         return raw_stream
     stream = AsyncStream(stream_cb=run_prompt, partial_deserializer=__partial_deserializer, final_deserializer=__deserializer)
     return stream
 
-BAMLContradictions.register_impl("version1")(version1, version1_stream)
+BAMLNoTipping.register_impl("version1")(version1, version1_stream)
