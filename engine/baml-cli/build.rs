8aa4fa68f1b9ad9846a3d0b46f0d03c5a5b075e6
Aaron Villalpando <aaron@boundaryml.com>
2024-04-18 12:50:37 -0700

    Change macosx target

--- engine/baml-cli/build.rs
+++ engine/baml-cli/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let target = std::env::var("TARGET").unwrap();
     if target.contains("apple") {
-        println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=11.0");
+        println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=10.13");
     }
 }
