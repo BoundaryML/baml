278cacc89099d95612d4ff76f484498a8ce49548
Sam Lijin <sam@boundaryml.com>
2024-04-23 16:57:29 -0700

    set up working gem

---
+++ engine/language_client_ruby/ext/baml/extconf.rb
@@ -0,0 +1,8 @@
+require "mkmf"
+require "rb_sys/mkmf"
+
+# create_rust_makefile takes, as its argument, ${CRATE_NAME}/${GEM_NAME} where:
+#
+#    - CRATE_NAME is the name of the crate in ext/${GEM_NAME}/Cargo.toml
+#    - GEM_NAME is the name of the gem in ext/baml
+create_rust_makefile("language_client_ruby/baml")
