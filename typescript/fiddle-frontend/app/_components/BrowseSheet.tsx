638c102e56f4717f84e7f98a1c48a38211d0893b
aaronvg <aaron@boundaryml.com>
2024-04-26 12:48:37 -0700

    Fix style on explore examples button (#599)

--- typescript/fiddle-frontend/app/_components/BrowseSheet.tsx
+++ typescript/fiddle-frontend/app/_components/BrowseSheet.tsx
@@ -4,17 +4,24 @@ import { SheetTrigger, SheetContent, Sheet } from '@/components/ui/sheet'
 import { Button } from '@/components/ui/button'
 import { Compass } from 'lucide-react'
 import { ExploreProjects } from '../[project_id]/_components/ExploreProjects'
-
+import { isMobile } from 'react-device-detect'
+import { useAtom } from 'jotai'
+import { exploreProjectsOpenAtom } from '../[project_id]/_atoms/atoms'
 export const BrowseSheet = () => {
+  const [open, setOpen] = useAtom(exploreProjectsOpenAtom)
+
+  if (isMobile) return null
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={() => setOpen(!open)}>
       <SheetTrigger asChild>
-        <Button className="flex flex-row items-center px-2 text-sm whitespace-pre-wrap bg-indigo-600 hover:bg-indigo-500 h-fit gap-x-2 text-vscode-button-foreground">
+        {/* Fake button, not used. the real button is in the actual project page.
+        We do this shit because if we add the Sheet to the page, and not the layout, the sheet will reset everytime the url changes to a new project, but we want it to be not re-render. Layouts aid in that. */}
+        <Button className="flex-row items-center hidden px-2 text-sm whitespace-no-wrap bg-indigo-600 hover:bg-indigo-500 h-fit gap-x-2 text-vscode-button-foreground">
           <Compass size={24} strokeWidth={2} />
           <span>Browse Examples</span>
         </Button>
       </SheetTrigger>
-      <SheetContent className="bg-zinc-900 min-w-[600px]">
+      <SheetContent className="bg-zinc-900 min-w-[600px]" onInteractOutside={() => setOpen(false)}>
         <ExploreProjects />
       </SheetContent>
     </Sheet>
--- typescript/playground-common/src/shared/FunctionPanel.tsx
+++ typescript/playground-common/src/shared/FunctionPanel.tsx
@@ -85,7 +85,7 @@ const FunctionPanel: React.FC = () => {
             <div className="w-full">
               <ResizablePanelGroup direction="horizontal" className="h-full">
                 {impl && (
-                  <ResizablePanel defaultSize={50} className="px-0 overflow-y-auto">
+                  <ResizablePanel defaultSize={60} className="px-0 overflow-y-auto">
                     <div className="relative w-full h-full overflow-y-auto">
                       {/* <ScrollArea type="auto" className="flex w-full h-full pr-3 "> */}
                       <div className="flex w-full h-full">{impls}</div>
@@ -94,7 +94,7 @@ const FunctionPanel: React.FC = () => {
                   </ResizablePanel>
                 )}
                 <ResizableHandle withHandle={false} className="bg-vscode-panel-border" />
-                <ResizablePanel minSize={30} className="pl-2 pr-0.5" hidden={!showTests}>
+                <ResizablePanel minSize={20} className="pl-2 pr-0.5" hidden={!showTests}>
                   {/* <Allotment.Pane className="pl-2 pr-0.5" minSize={200} visible={showTests}> */}
                   <div className="flex flex-col h-full overflow-y-auto overflow-x-clip">
                     {/* On windows this scroll area extends beyond the wanted width, so we just use a normal scrollbar here vs using ScrollArea*/}
