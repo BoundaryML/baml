ddf77d87540011a036ee99e3d55aa9e548527af4
Sam Lijin <sam@boundaryml.com>
2024-04-25 22:59:47 -0700

    save async test

---
+++ engine/language_client_ruby/test/async.rb
@@ -0,0 +1,32 @@
+require "async"
+require_relative "../lib/baml"
+
+start = Time.now
+
+Async do |task|
+  #task.async do
+  #  puts "BEGIN- ruby-native sleep"
+  #  sleep 1
+  #  puts "END- ruby-native sleep"
+  #end
+  task.async do
+      Baml.hello_from_rust()
+  end
+  task.async do
+      Baml.hello_from_rust()
+  end
+  task.async do
+  #  Fiber.schedule do
+  #    puts "a-put1"
+  #    #Fiber.yield
+  #    puts "a-put2"
+  #  end
+  #  Fiber.schedule do
+  #    puts "b-put1"
+  #    #Fiber.yield
+  #    puts "b-put2"
+  #  end
+  end
+end
+
+puts "Duration: #{Time.now - start}"
