0111785172b29dcaa168a85b38b1715d5fd27522
Sam Lijin <sam@boundaryml.com>
2024-04-30 11:10:20 -0700

    fix ruby convert_test

--- engine/language_client_ruby/test/convert_test.rb
+++ engine/language_client_ruby/test/convert_test.rb
@@ -80,14 +80,6 @@ BOOK_HASH = {
 }
 
 describe "converting Ruby objects to JSON" do
-  it "BamlConvert handles JSON to Ruby" do
-    converted = Baml::convert_to(Book).from(BOOK_HASH)
-    assert_equal(converted, BOOK_OBJ)
-
-    converted = Baml::convert_to(BookGenre).from("NON_FICTION")
-    assert_equal(converted, BookGenre::NON_FICTION)
-  end
-
   it "T::Struct.serialize does not handle unions correctly" do
     refute_equal(
       BOOK_OBJ.serialize,
@@ -130,14 +122,14 @@ describe "converting Ruby objects to JSON" do
 end
 
 describe "converting JSON to Ruby objects" do
-  it "handles complex objects" do
-    skip "unions of types are not handled correctly"
+  it "BamlConvert handles JSON to Ruby" do
+    converted = Baml::convert_to(Book).from(BOOK_HASH)
+    assert_equal(converted, BOOK_OBJ)
 
-    assert_equal(
-      TypeCoerce[Book].new.from(BOOK_HASH),
-      BOOK_OBJ
-    )
+    converted = Baml::convert_to(BookGenre).from("NON_FICTION")
+    assert_equal(converted, BookGenre::NON_FICTION)
   end
+
 
   it "parses enums" do
     assert_equal(
