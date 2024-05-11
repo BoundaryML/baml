032607bc8e3b33c6926f88b3c06e98cc53fa03a9
Sam Lijin <sam@boundaryml.com>
2024-05-10 18:49:33 -0700

    put everything in dist when building

--- engine/language_client_typescript/test.ts
+++ engine/language_client_typescript/test.ts
@@ -1,5 +1,9 @@
+import { BamlRuntimeFfi, rustIsInstance } from './index'
 import { BamlClient } from './integ-tests-ts-v2/client'
 ;(async () => {
+  const x = BamlRuntimeFfi.fromDirectory('/home/sam/baml/integ-tests/baml_src')
+  console.log('rust-based isinstance ', rustIsInstance(x))
+
   const b = BamlClient.fromDirectory('/home/sam/baml/integ-tests/baml_src')
 
   const result = await b.ExtractNames({ input: 'hello this is patrick' })
