46825b11d9246ee443749df99ff098e7c9d05ec1
Aaron Villalpando <aaron@boundaryml.com>
2024-05-15 13:47:17 -0500

    Fix test results not showing up correctly in the table

--- typescript/playground-common/src/baml_wasm_web/test_uis/testHooks.ts
+++ typescript/playground-common/src/baml_wasm_web/test_uis/testHooks.ts
@@ -1,6 +1,6 @@
 import { atom, useAtomValue } from 'jotai'
 import { atomFamily, useAtomCallback } from 'jotai/utils'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { runtimeCtx, selectedFunctionAtom, selectedRuntimeAtom } from '../EventListener'
 import type { WasmFunctionResponse, WasmTestResponse } from '@gloo-ai/baml-schema-wasm-web/baml_schema_build'
 
@@ -28,9 +28,12 @@ export type TestState =
       message: string
     }
 
-const statusAtom = atom<TestState>({ status: 'queued' })
+// const statusAtom = atom<TestState>({ status: 'queued' })
 
-export const testStatusAtom = atomFamily((testName: string) => statusAtom)
+export const testStatusAtom = atomFamily(
+  (testName: string) => atom<TestState>({ status: 'queued' }),
+  (a, b) => a === b,
+)
 export const runningTestsAtom = atom<string[]>([])
 export const statusCountAtom = atom({
   queued: 0,
@@ -47,121 +50,128 @@ export const statusCountAtom = atom({
 export const useRunHooks = () => {
   const isRunning = useAtomValue(isRunningAtom)
 
-  const runTest = useAtomCallback(async (get, set, testNames: string[]) => {
-    const runtime = get(selectedRuntimeAtom)
-    const func = get(selectedFunctionAtom)
-    if (!runtime || !func) {
-      // Refuse to run a test if no runtime is selected
-      return
-    }
-    const isRunning = get(isRunningAtom)
-    if (isRunning) {
-      // Refuse to run another test if one is already running
-      return
-    }
-    set(isRunningAtom, true)
-    set(showTestsAtom, true)
-
-    const ctx = get(runtimeCtx)
-    console.log('test baml getting runtime context')
-    // First clear any previous test results
-    testStatusAtom.setShouldRemove(() => true)
-    // Remove the shouldRemove function so we don't remove future test results
-    testStatusAtom.setShouldRemove(null)
-
-    set(runningTestsAtom, testNames)
-    set(statusCountAtom, {
-      queued: testNames.length,
-      running: 0,
-      done: {
-        passed: 0,
-        llm_failed: 0,
-        parse_failed: 0,
-        error: 0,
-      },
-      error: 0,
-    })
-
-    // TODO: @hellovai find out why large batch sizes don't work
-    const batchSize = 1
-    const batches = []
-    for (let i = 0; i < testNames.length; i += batchSize) {
-      batches.push(testNames.slice(i, i + batchSize))
-    }
-    for (const batch of batches) {
-      const promises = await Promise.allSettled(
-        batch.map((testName) => {
-          set(testStatusAtom(testName), { status: 'running' })
-          set(statusCountAtom, (prev) => {
-            return {
-              ...prev,
-              running: prev.running + 1,
-              queued: prev.queued - 1,
-            }
-          })
-          if (!func || !runtime || !ctx) {
-            return Promise.reject(new Error('Code potentially modified while running tests'))
-          }
-          let now = new Date().getTime()
-          return func
-            .run_test(runtime, ctx, testName, (intermediate: WasmFunctionResponse) => {
-              set(testStatusAtom(testName), {
-                status: 'running',
-                response: intermediate,
-              })
-            })
-            .then((res) => {
-              let elapsed = new Date().getTime() - now
-              return { res, elapsed }
-            })
-        }),
-      )
-      for (let i = 0; i < promises.length; i++) {
-        const result = promises[i]
-        if (result.status === 'fulfilled') {
-          const { res, elapsed } = result.value
-          let status = res.status()
-          let response_status: DoneTestStatusType = 'error'
-          if (status === 0) {
-            response_status = 'passed'
-          } else if (status === 1) {
-            response_status = 'llm_failed'
-          } else if (status === 2) {
-            response_status = 'parse_failed'
-          } else {
-            response_status = 'error'
-          }
-          set(testStatusAtom(batch[i]), {
-            status: 'done',
-            response_status,
-            response: res,
-            latency_ms: elapsed,
-          })
-          set(statusCountAtom, (prev) => {
-            return {
-              ...prev,
-              done: {
-                ...prev.done,
-                [response_status]: prev.done[response_status] + 1,
-              },
-              running: prev.running - 1,
-            }
-          })
-        } else {
-          set(testStatusAtom(batch[i]), { status: 'error', message: `${result.reason}` })
-          set(statusCountAtom, (prev) => {
-            return {
-              ...prev,
-              error: prev.error + 1,
-              running: prev.running - 1,
-            }
-          })
+  const runTest = useAtomCallback(
+    useCallback(
+      async (get, set, testNames: string[]) => {
+        const runtime = get(selectedRuntimeAtom)
+        const func = get(selectedFunctionAtom)
+        if (!runtime || !func) {
+          // Refuse to run a test if no runtime is selected
+          return
         }
-      }
-    }
+        const isRunning = get(isRunningAtom)
+        if (isRunning) {
+          // Refuse to run another test if one is already running
+          return
+        }
+        set(isRunningAtom, true)
+        set(showTestsAtom, true)
 
-    set(isRunningAtom, false)
-  })
+        const ctx = get(runtimeCtx)
+        console.log('test baml getting runtime context')
+        // First clear any previous test results
+        testStatusAtom.setShouldRemove(() => true)
+        // Remove the shouldRemove function so we don't remove future test results
+        testStatusAtom.setShouldRemove(null)
+
+        set(runningTestsAtom, testNames)
+        set(statusCountAtom, {
+          queued: testNames.length,
+          running: 0,
+          done: {
+            passed: 0,
+            llm_failed: 0,
+            parse_failed: 0,
+            error: 0,
+          },
+          error: 0,
+        })
+
+        // TODO: @hellovai find out why large batch sizes don't work
+        const batchSize = 1
+        const batches = []
+        for (let i = 0; i < testNames.length; i += batchSize) {
+          batches.push(testNames.slice(i, i + batchSize))
+        }
+        for (const batch of batches) {
+          const promises = await Promise.allSettled(
+            batch.map(async (testName) => {
+              set(testStatusAtom(testName), { status: 'running' })
+              set(statusCountAtom, (prev) => {
+                return {
+                  ...prev,
+                  running: prev.running + 1,
+                  queued: prev.queued - 1,
+                }
+              })
+              if (!func || !runtime || !ctx) {
+                return Promise.reject(new Error('Code potentially modified while running tests'))
+              }
+              let now = new Date().getTime()
+              return func
+                .run_test(runtime, ctx, testName, (intermediate: WasmFunctionResponse) => {
+                  set(testStatusAtom(testName), {
+                    status: 'running',
+                    response: intermediate,
+                  })
+                })
+                .then((res) => {
+                  let elapsed = new Date().getTime() - now
+                  return { res, elapsed }
+                })
+            }),
+          )
+          for (let i = 0; i < promises.length; i++) {
+            const result = promises[i]
+            if (result.status === 'fulfilled') {
+              const { res, elapsed } = result.value
+              // console.log('result', i, result.value.res.llm_response(), 'batch[i]', batch[i])
+
+              let status = res.status()
+              let response_status: DoneTestStatusType = 'error'
+              if (status === 0) {
+                response_status = 'passed'
+              } else if (status === 1) {
+                response_status = 'llm_failed'
+              } else if (status === 2) {
+                response_status = 'parse_failed'
+              } else {
+                response_status = 'error'
+              }
+              set(testStatusAtom(batch[i]), {
+                status: 'done',
+                response_status,
+                response: res,
+                latency_ms: elapsed,
+              })
+              set(statusCountAtom, (prev) => {
+                return {
+                  ...prev,
+                  done: {
+                    ...prev.done,
+                    [response_status]: prev.done[response_status] + 1,
+                  },
+                  running: prev.running - 1,
+                }
+              })
+            } else {
+              set(testStatusAtom(batch[i]), { status: 'error', message: `${result.reason}` })
+              set(statusCountAtom, (prev) => {
+                return {
+                  ...prev,
+                  error: prev.error + 1,
+                  running: prev.running - 1,
+                }
+              })
+            }
+          }
+        }
+
+        set(isRunningAtom, false)
+      },
+      [[isRunningAtom, selectedRuntimeAtom, selectedFunctionAtom, runtimeCtx]],
+    ),
+  )
 
   return { isRunning, run: runTest }
 }
