171cbfe96c2f8cadf96987cad4d349d9b0273448
hellovai <vbv@boundaryml.com>
2024-06-03 16:34:45 -0400

    Fix issue with tracing in threadpool executors (#639)
    * When using AsyncContextVars + ThreadPoolExecutors, things can get
    quite hairy. In BAML we treat threadpool executors as top level threads
    with no prior context (i.e. things like tags and such are reset and must
    be re-configured)
    * See:
    https://kobybass.medium.com/python-contextvars-and-multithreading-faa33dbe953d

--- engine/language_client_python/python_src/baml_py/async_context_vars.py
+++ engine/language_client_python/python_src/baml_py/async_context_vars.py
@@ -8,41 +8,59 @@ import inspect
 import typing
 from .baml_py import RuntimeContextManager, BamlRuntime, BamlSpan
 import atexit
+import threading
 
 F = typing.TypeVar("F", bound=typing.Callable[..., typing.Any])
+
+
+# See this article about why we need to track for every thread:
+# https://kobybass.medium.com/python-contextvars-and-multithreading-faa33dbe953d
+RTContextVar = contextvars.ContextVar[typing.Dict[int, RuntimeContextManager]]
+
+
+def current_thread_id() -> int:
+    current_thread = threading.current_thread()
+    return current_thread.native_id or 0
 
 
 class CtxManager:
     def __init__(self, rt: BamlRuntime):
         self.rt = rt
-        self.ctx = contextvars.ContextVar[RuntimeContextManager](
-            "baml_ctx", default=rt.create_context_manager()
+        self.ctx = contextvars.ContextVar[typing.Dict[int, RuntimeContextManager]](
+            "baml_ctx", default={current_thread_id(): rt.create_context_manager()}
         )
         atexit.register(self.rt.flush)
 
+    def __ctx(self) -> RuntimeContextManager:
+        ctx = self.ctx.get()
+        thread_id = current_thread_id()
+        if thread_id not in ctx:
+            ctx[thread_id] = self.rt.create_context_manager()
+        return ctx[thread_id]
+
     def upsert_tags(self, **tags: str) -> None:
-        mngr = self.ctx.get()
+        mngr = self.__ctx()
         mngr.upsert_tags(tags)
 
     def get(self) -> RuntimeContextManager:
-        return self.ctx.get()
+        return self.__ctx()
 
     def start_trace_sync(
         self, name: str, args: typing.Dict[str, typing.Any]
     ) -> BamlSpan:
-        mng = self.ctx.get()
+        mng = self.__ctx()
         return BamlSpan.new(self.rt, name, args, mng)
 
     def start_trace_async(
         self, name: str, args: typing.Dict[str, typing.Any]
     ) -> BamlSpan:
-        mng = self.ctx.get()
+        mng = self.__ctx()
         cln = mng.deep_clone()
-        self.ctx.set(cln)
+        self.ctx.set({current_thread_id(): cln})
         return BamlSpan.new(self.rt, name, args, cln)
 
     def end_trace(self, span: BamlSpan, response: typing.Any) -> None:
-        span.finish(response, self.ctx.get())
+        span.finish(response, self.__ctx())
 
     def flush(self) -> None:
         self.rt.flush()
