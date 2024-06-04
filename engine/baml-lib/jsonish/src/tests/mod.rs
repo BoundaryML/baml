9abc98748c7aff0c0354cdd821e44bab93eed46c
Aaron Villalpando <aaron@boundaryml.com>
2024-06-03 23:26:44 -0700

    Add failed parsing test

--- engine/baml-lib/jsonish/src/tests/mod.rs
+++ engine/baml-lib/jsonish/src/tests/mod.rs
@@ -361,3 +361,201 @@ test_deserializer!(
     FieldType::Class("Foo".to_string()),
     json!({"id": r#"{{hi} there"# })
 );
+
+
+const EXAMPLE_FILE: &str = r##"
+class Score {
+    year int @description(#"
+      The year you're giving the score for.
+    "#)
+    score int @description(#"
+      1 to 100
+    "#)
+  }
+  
+  class PopularityOverTime {
+    bookName string
+    scores Score[]
+  }
+  
+  class WordCount {
+    bookName string
+    count int
+  }
+  
+  class Ranking {
+    bookName string
+    score int @description(#"
+      1 to 100 of your own personal score of this book
+    "#)
+  }
+   
+  class BookAnalysis {
+    bookNames string[] @description(#"
+      The list of book names  provided
+    "#)
+    popularityOverTime PopularityOverTime[] @description(#"
+      Print the popularity of EACH BOOK over time.
+    "#) @alias(popularityData)
+    // popularityRankings Ranking[] @description(#"
+    //   A list of the book's popularity rankings over time. 
+    //   The first element is the top ranking
+    // "#)
+   
+    // wordCounts WordCount[]
+  }
+"##;  
+
+test_deserializer!(
+    test_string_from_string25,
+    EXAMPLE_FILE,
+    r#"
+    {
+        "bookNames": ["brave new world", "the lord of the rings", "three body problem", "stormlight archive"],
+        "popularityData": [
+          {
+            "bookName": "brave new world",
+            "scores": [
+              {
+                "year": 1932,
+                "score": 65
+              },
+              {
+                "year": 2000,
+                "score": 80
+              },
+              {
+                "year": 2021,
+                "score": 70
+              }
+            ]
+          },
+          {
+            "bookName": "the lord of the rings",
+            "scores": [
+              {
+                "year": 1954,
+                "score": 75
+              },
+              {
+                "year": 2001,
+                "score": 95
+              },
+              {
+                "year": 2021,
+                "score": 90
+              }
+            ]
+          },
+          {
+            "bookName": "three body problem",
+            "scores": [
+              {
+                "year": 2008,
+                "score": 60
+              },
+              {
+                "year": 2014,
+                "score": 79
+              },
+              {
+                "year": 2021,
+                "score": 85
+              }
+            ]
+          },
+          {
+            "bookName": "stormlight archive",
+            "scores": [
+              {
+                "year": 2010,
+                "score": 76
+              },
+              {
+                "year": 2020,
+                "score": 85
+              },
+              {
+                "year": 2021,
+                "score": 81
+              }
+            ]
+          }
+        ]
+      }
+    "#,
+    FieldType::Class("BookAnalysis".to_string()),
+    json!({
+        "bookNames": ["brave new world", "the lord of the rings", "three body problem", "stormlight archive"],
+        "popularityOverTime": [
+          {
+            "bookName": "brave new world",
+            "scores": [
+              {
+                "year": 1932,
+                "score": 65
+              },
+              {
+                "year": 2000,
+                "score": 80
+              },
+              {
+                "year": 2021,
+                "score": 70
+              }
+            ]
+          },
+          {
+            "bookName": "the lord of the rings",
+            "scores": [
+              {
+                "year": 1954,
+                "score": 75
+              },
+              {
+                "year": 2001,
+                "score": 95
+              },
+              {
+                "year": 2021,
+                "score": 90
+              }
+            ]
+          },
+          {
+            "bookName": "three body problem",
+            "scores": [
+              {
+                "year": 2008,
+                "score": 60
+              },
+              {
+                "year": 2014,
+                "score": 79
+              },
+              {
+                "year": 2021,
+                "score": 85
+              }
+            ]
+          },
+          {
+            "bookName": "stormlight archive",
+            "scores": [
+              {
+                "year": 2010,
+                "score": 76
+              },
+              {
+                "year": 2020,
+                "score": 85
+              },
+              {
+                "year": 2021,
+                "score": 81
+              }
+            ]
+          }
+        ]
+      })  
+);
