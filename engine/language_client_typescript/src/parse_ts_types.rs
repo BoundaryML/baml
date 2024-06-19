ea340766fb197340b45126d4f9031c62e24c8a49
Anish Palakurthi <anish.palakurthi@gmail.com>
2024-06-18 17:45:40 -0700

    added ts integ test

--- engine/language_client_typescript/src/parse_ts_types.rs
+++ engine/language_client_typescript/src/parse_ts_types.rs
@@ -11,6 +11,7 @@ use napi::JsString;
 use napi::JsUnknown;
 use napi::NapiRaw;
 
+use crate::types::audio::BamlAudio;
 use crate::types::image::BamlImage;
 
 struct SerializationError {
@@ -194,6 +195,8 @@ pub fn jsunknown_to_baml_value(
             let external = unsafe { item.cast::<JsExternal>() };
             if let Ok(img) = env.get_value_external::<BamlImage>(&external) {
                 BamlValue::Media(img.inner.clone())
+            } else if let Ok(audio) = env.get_value_external::<BamlAudio>(&external) {
+                BamlValue::Media(audio.inner.clone())
             } else {
                 if skip_unsupported {
                     return Ok(None);
--- engine/language_client_typescript/typescript_src/index.ts
+++ engine/language_client_typescript/typescript_src/index.ts
@@ -1,3 +1,3 @@
-export { BamlRuntime, FunctionResult, FunctionResultStream, BamlImage as Image, invoke_runtime_cli } from './native'
+export { BamlRuntime, FunctionResult, FunctionResultStream, BamlImage as Image, BamlAudio as Audio, invoke_runtime_cli } from './native'
 export { BamlStream } from './stream'
 export { CtxManager as BamlCtxManager } from './async_context_vars'
