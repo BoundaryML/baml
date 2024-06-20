3d55217bf16900e1ce52ff57e2744c49f691f33f
Anish Palakurthi <anish.palakurthi@gmail.com>
2024-06-20 16:39:48 -0700

    replaced proxy url

--- typescript/playground-common/src/baml_wasm_web/EventListener.tsx
+++ typescript/playground-common/src/baml_wasm_web/EventListener.tsx
@@ -21,7 +21,7 @@ const defaultEnvKeyValues: [string, string][] = (() => {
       // we can do somehting fancier here later if we want to test locally.
       return [['BOUNDARY_PROXY_URL', 'https://fiddle-proxy.fly.dev']]
     }
-    return [['BOUNDARY_PROXY_URL', 'https://localhost:3000']]
+    return [['BOUNDARY_PROXY_URL', 'https://fiddle-proxy.fly.dev']]
   } else {
     console.log('Not running in a Next.js environment, set default value')
     // Not running in a Next.js environment, set default value
