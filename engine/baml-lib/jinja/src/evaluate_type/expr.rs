48727ce5e08bb33b615dad7bf3b3bfeacd7db400
Vaibhav Gupta <vbv@boundaryml.com>
2024-06-29 02:57:11 -0700

    Add improved static analysis for jinja
    * Includes better testing for ctx.output_format,
      unions, filters

--- engine/baml-lib/jinja/src/evaluate_type/expr.rs
+++ engine/baml-lib/jinja/src/evaluate_type/expr.rs
@@ -8,6 +8,90 @@ use super::{
     ScopeTracker, TypeError,
 };
 
+fn parse_as_function_call<'a>(
+    expr: &ast::Spanned<ast::Call>,
+    state: &mut ScopeTracker,
+    types: &PredefinedTypes,
+    t: &Type,
+) -> (Type, Vec<TypeError>) {
+    match t {
+        Type::FunctionRef(name) => {
+            let mut positional_args = Vec::new();
+            let mut kwargs = HashMap::new();
+            for arg in &expr.args {
+                match arg {
+                    ast::Expr::Kwargs(kkwargs) => {
+                        for (k, v) in &kkwargs.pairs {
+                            let t = tracker_visit_expr(v, state, types);
+                            kwargs.insert(*k, t);
+                        }
+                    }
+                    _ => {
+                        let t = tracker_visit_expr(arg, state, types);
+                        positional_args.push(t);
+                    }
+                }
+            }
+
+            types.check_function_args((&name, expr), &positional_args, &kwargs)
+        }
+        Type::Both(x, y) => {
+            match (x.as_ref(), y.as_ref()) {
+                (Type::FunctionRef(_), Type::FunctionRef(_)) => {}
+                (Type::FunctionRef(_), _) => return parse_as_function_call(expr, state, types, x),
+                (_, Type::FunctionRef(_)) => return parse_as_function_call(expr, state, types, y),
+                _ => {}
+            }
+
+            let (t1, e1) = parse_as_function_call(expr, state, types, x);
+            let (t2, e2) = parse_as_function_call(expr, state, types, y);
+            match (e1.is_empty(), e2.is_empty()) {
+                (true, true) => (Type::merge([t1, t2]), vec![]),
+                (true, false) => (t1, e1),
+                (false, true) => (t2, e2),
+                (false, false) => (
+                    Type::merge([t1, t2]),
+                    e1.into_iter().chain(e2.into_iter()).collect(),
+                ),
+            }
+        }
+        Type::Union(items) => {
+            let items = items
+                .iter()
+                .map(|x| parse_as_function_call(expr, state, types, x))
+                .reduce(|acc, x| {
+                    let (t1, e1) = acc;
+                    let (t2, e2) = x;
+                    (
+                        Type::merge([t1, t2]),
+                        e1.into_iter().chain(e2.into_iter()).collect(),
+                    )
+                });
+            match items {
+                Some(x) => x,
+                None => (
+                    Type::Unknown,
+                    vec![TypeError::new_invalid_type(
+                        &expr.expr,
+                        t,
+                        "function",
+                        expr.span(),
+                    )],
+                ),
+            }
+        }
+        _ => (
+            Type::Unknown,
+            vec![TypeError::new_invalid_type(
+                &expr.expr,
+                t,
+                "function",
+                expr.span(),
+            )],
+        ),
+    }
+}
+
 fn tracker_visit_expr<'a>(
     expr: &ast::Expr<'a>,
     state: &mut ScopeTracker,
@@ -79,16 +163,145 @@ fn tracker_visit_expr<'a>(
                 .unwrap_or(Type::Unknown);
             Type::merge([true_expr, false_expr])
         }
-        ast::Expr::Filter(expr) => match expr.name {
-            "items" => {
-                let inner = tracker_visit_expr(expr.expr.as_ref().unwrap(), state, types);
-                match inner {
+        ast::Expr::Filter(expr) => {
+            // Filters have a name
+            let inner = tracker_visit_expr(expr.expr.as_ref().unwrap(), state, types);
+
+            let mut ensure_type = |error_string: &str| {
+                state.errors.push(TypeError::new_invalid_type(
+                    expr.expr.as_ref().unwrap(),
+                    &inner,
+                    error_string,
+                    expr.span(),
+                ));
+            };
+
+            let valid_filters = vec![
+                "abs",
+                "attrs",
+                "batch",
+                "bool",
+                "capitalize",
+                "escape",
+                "first",
+                "last",
+                "default",
+                "float",
+                "indent",
+                "int",
+                "dictsort",
+                "items",
+                "join",
+                "length",
+                "list",
+                "lower",
+                "upper",
+                "map",
+                "max",
+                "min",
+                "pprint",
+                "reject",
+                "rejectattr",
+                "replace",
+                "reverse",
+                "round",
+                "safe",
+                "select",
+                "selectattr",
+                "slice",
+                "sort",
+                "split",
+                "title",
+                "tojson",
+                "json",
+                "trim",
+                "unique",
+                "urlencode",
+            ];
+            match expr.name {
+                "abs" => {
+                    if inner.matches(&Type::Number) {
+                        ensure_type("number");
+                    }
+                    Type::Number
+                }
+                "attrs" => Type::Unknown,
+                "batch" => Type::Unknown,
+                "bool" => Type::Bool,
+                "capitalize" | "escape" => {
+                    if inner.matches(&Type::String) {
+                        ensure_type("string");
+                    }
+                    Type::String
+                }
+                "first" | "last" => match inner {
+                    Type::List(t) => Type::merge([*t, Type::None]),
+                    Type::Unknown => Type::Unknown,
+                    _ => {
+                        ensure_type("list");
+                        Type::Unknown
+                    }
+                },
+                "default" => Type::Unknown,
+                "float" => Type::Float,
+                "indent" => Type::String,
+                "int" => Type::Int,
+                "dictsort" | "items" => match inner {
                     Type::Map(k, v) => Type::List(Box::new(Type::Tuple(vec![*k, *v]))),
-                    _ => Type::Unknown,
+                    Type::ClassRef(_) => {
+                        Type::List(Box::new(Type::Tuple(vec![Type::String, Type::Unknown])))
+                    }
+                    _ => {
+                        ensure_type("map or class");
+                        Type::Unknown
+                    }
+                },
+                "join" => Type::String,
+                "length" => match inner {
+                    Type::List(_) | Type::String | Type::ClassRef(_) | Type::Map(_, _) => Type::Int,
+                    Type::Unknown => Type::Unknown,
+                    _ => {
+                        ensure_type("list, string, class or map");
+                        Type::Unknown
+                    }
+                },
+                "list" => Type::List(Box::new(Type::Unknown)),
+                "lower" | "upper" => {
+                    if inner.matches(&Type::String) {
+                        ensure_type("string");
+                    }
+                    Type::String
+                }
+                "map" => Type::Unknown,
+                "max" => Type::Unknown,
+                "min" => Type::Unknown,
+                "pprint" => Type::Unknown,
+                "reject" => Type::Unknown,
+                "rejectattr" => Type::Unknown,
+                "replace" => Type::String,
+                "reverse" => Type::Unknown,
+                "round" => Type::Float,
+                "safe" => Type::String,
+                "select" => Type::Unknown,
+                "selectattr" => Type::Unknown,
+                "slice" => Type::Unknown,
+                "sort" => Type::Unknown,
+                "split" => Type::List(Box::new(Type::String)),
+                "title" => Type::String,
+                "tojson" | "json" => Type::String,
+                "trim" => Type::String,
+                "unique" => Type::Unknown,
+                "urlencode" => Type::String,
+                other => {
+                    state.errors.push(TypeError::new_invalid_filter(
+                        other,
+                        expr.span(),
+                        &valid_filters,
+                    ));
+                    Type::Unknown
                 }
             }
-            _ => Type::Unknown,
-        },
+        }
         ast::Expr::Test(expr) => {
             let _test = tracker_visit_expr(&expr.expr, state, types);
             // TODO: Check for type compatibility
@@ -122,41 +335,9 @@ fn tracker_visit_expr<'a>(
         ast::Expr::Slice(_slice) => Type::Unknown,
         ast::Expr::Call(expr) => {
             let func = tracker_visit_expr(&expr.expr, state, types);
-
-            match func {
-                Type::FunctionRef(name) => {
-                    // lets segregate positional and keyword arguments
-                    let mut positional_args = Vec::new();
-                    let mut kwargs = HashMap::new();
-                    for arg in &expr.args {
-                        match arg {
-                            ast::Expr::Kwargs(kkwargs) => {
-                                for (k, v) in &kkwargs.pairs {
-                                    let t = tracker_visit_expr(v, state, types);
-                                    kwargs.insert(*k, t);
-                                }
-                            }
-                            _ => {
-                                let t = tracker_visit_expr(arg, state, types);
-                                positional_args.push(t);
-                            }
-                        }
-                    }
-
-                    let res = types.check_function_args((&name, expr), &positional_args, &kwargs);
-                    state.errors.extend(res.1);
-                    res.0
-                }
-                t => {
-                    state.errors.push(TypeError::new_invalid_type(
-                        &expr.expr,
-                        &t,
-                        "function",
-                        expr.span(),
-                    ));
-                    Type::Unknown
-                }
-            }
+            let (t, errs) = parse_as_function_call(expr, state, types, &func);
+            state.errors.extend(errs);
+            t
         }
         ast::Expr::List(expr) => {
             let inner = Type::merge(
