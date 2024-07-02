8e1dc1e4a4937d9ffb801b379f40ce3788f8e35c
Sam Lijin <sam@boundaryml.com>
2024-07-02 16:53:54 -0700

    make it compile pt1

--- engine/baml-runtime/src/request/mod.rs
+++ engine/baml-runtime/src/request/mod.rs
@@ -4,18 +4,16 @@ use web_time::Duration;
 fn builder() -> reqwest::ClientBuilder {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
-            let cb = reqwest::Client::builder();
+            reqwest::Client::builder();
         } else {
-            let cb = reqwest::Client::builder()
-                .http2_keep_alive_interval(Some(Duration::from_secs(10)));
-
+            reqwest::Client::builder()
+                // NB: we can NOT set a total request timeout here: our users
+                // regularly have requests that take multiple minutes, due to how
+                // long LLMs take
+                .connect_timeout(Duration::from_secs(10))
+                .http2_keep_alive_interval(Some(Duration::from_secs(10)))
         }
     }
-
-    // NB: we can NOT set a total request timeout here: our users
-    // regularly have requests that take multiple minutes, due to how
-    // long LLMs take
-    cb.connect_timeout(Duration::from_secs(10))
 }
 
 pub(crate) fn create_client() -> Result<reqwest::Client> {
@@ -23,9 +21,14 @@ pub(crate) fn create_client() -> Result<reqwest::Client> {
 }
 
 pub(crate) fn create_tracing_client() -> Result<reqwest::Client> {
-    builder()
-        // Wait up to 30s to send traces to the backend
-        .read_timeout(Duration::from_secs(30))
-        .build()
+    if #[cfg(target_arch = "wasm32")] {
+        let cb = builder();
+    } else {
+        let cb =builder()
+            // Wait up to 30s to send traces to the backend
+            .read_timeout(Duration::from_secs(30));
+    }
+
+    cb.build()
         .context("Failed to create reqwest client")
 }
