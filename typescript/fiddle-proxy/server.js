9ed647952531fe093aaf3e9484652615e0aac825
Sam Lijin <sam@boundaryml.com>
2024-07-10 19:01:27 -0700

    check against origin, not startsWith

--- typescript/fiddle-proxy/server.js
+++ typescript/fiddle-proxy/server.js
@@ -6,7 +6,26 @@ require('dotenv').config()
 
 app.use(cors())
 
-// These are the domains which we may "leak" our API keys to.
+// From https://nodejs.org/api/url.html#url-strings-and-url-objects:
+// ┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+// │                                              href                                              │
+// ├──────────┬──┬─────────────────────┬────────────────────────┬───────────────────────────┬───────┤
+// │ protocol │  │        auth         │          host          │           path            │ hash  │
+// │          │  │                     ├─────────────────┬──────┼──────────┬────────────────┤       │
+// │          │  │                     │    hostname     │ port │ pathname │     search     │       │
+// │          │  │                     │                 │      │          ├─┬──────────────┤       │
+// │          │  │                     │                 │      │          │ │    query     │       │
+// "  https:   //    user   :   pass   @ sub.example.com : 8080   /p/a/t/h  ?  query=string   #hash "
+// │          │  │          │          │    hostname     │ port │          │                │       │
+// │          │  │          │          ├─────────────────┴──────┤          │                │       │
+// │ protocol │  │ username │ password │          host          │          │                │       │
+// ├──────────┴──┼──────────┴──────────┼────────────────────────┤          │                │       │
+// │   origin    │                     │         origin         │ pathname │     search     │ hash  │
+// ├─────────────┴─────────────────────┴────────────────────────┴──────────┴────────────────┴───────┤
+// │                                              href                                              │
+// └────────────────────────────────────────────────────────────────────────────────────────────────┘
+
+// These are the origins which we may "leak" our API keys to.
 //
 // We inject our API keys into requests to these domains so that promptfiddle users are not
 // required to provide their own API keys, but we must make sure that these API keys cannot be
@@ -16,18 +35,16 @@ app.use(cors())
 // override the base_url of any client, this allowlist guarantees that we only inject API keys
 // in requests to these model providers.
 const API_KEY_INJECTION_ALLOWED = {
-  'https://api.openai.com/': { Authorization: `Bearer ${process.env.OPENAI_API_KEY}` },
-  'https://api.anthropic.com/': { 'x-api-key': process.env.ANTHROPIC_API_KEY },
-  'https://generativelanguage.googleapis.com/': { 'x-goog-api-key': process.env.GOOGLE_API_KEY },
+  'https://api.openai.com': { Authorization: `Bearer ${process.env.OPENAI_API_KEY}` },
+  'https://api.anthropic.com': { 'x-api-key': process.env.ANTHROPIC_API_KEY },
+  'https://generativelanguage.googleapis.com': { 'x-goog-api-key': process.env.GOOGLE_API_KEY },
 }
 
 // Consult sam@ before changing this.
 for (const url of Object.keys(API_KEY_INJECTION_ALLOWED)) {
   assert(
-    // The trailing slash is important! Otherwise, users could bypass the allowlist using
-    // a subdomain like https://api.openai.com.evil.com/
-    url === `https://${new URL(url).hostname}/`,
-    `Keys of API_KEY_INJECTION_ALLOWED must be root HTTPS URLs for model providers, got ${url}`,
+    url === new URL(url).origin && new URL(url).protocol === 'https:',
+    `Keys of API_KEY_INJECTION_ALLOWED must be HTTPS origins for model providers, got ${url}`,
   )
 }
 
@@ -54,12 +71,25 @@ app.use(
     logger: console,
     on: {
       proxyReq: (proxyReq, req, res) => {
-        for (const [url, headers] of Object.entries(API_KEY_INJECTION_ALLOWED)) {
-          if (req.headers['baml-original-url'] == url) {
-            for (const [header, value] of Object.entries(headers)) {
-              proxyReq.setHeader(header, value)
-            }
+        try {
+          const bamlOriginalUrl = req.headers['baml-original-url']
+          if (bamlOriginalUrl === undefined) {
+            return
           }
+          const proxyOrigin = new URL(bamlOriginalUrl).origin
+          // It is very important that we ONLY resolve against API_KEY_INJECTION_ALLOWED
+          // by using the URL origin! (i.e. NOT using str.startsWith - the latter can still
+          // leak API keys to malicious subdomains e.g. https://api.openai.com.evil.com
+          const headers = API_KEY_INJECTION_ALLOWED[proxyOrigin]
+          if (headers === undefined) {
+            return
+          }
+          for (const [header, value] of Object.entries(headers)) {
+            proxyReq.setHeader(header, value)
+          }
+        } catch (err) {
+          // This is not console.warn because it's not important
+          console.log('baml-original-url is not parsable', err)
         }
       },
       proxyRes: (proxyRes, req, res) => {
