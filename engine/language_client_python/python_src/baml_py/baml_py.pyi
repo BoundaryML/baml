c3dff627fc4b82a63454e9e3f6c4587386f37356
Vaibhav Gupta <vbv@boundaryml.com>
2024-07-22 17:00:39 -0700

    better udpates

--- engine/language_client_python/python_src/baml_py/baml_py.pyi
+++ engine/language_client_python/python_src/baml_py/baml_py.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, List, Optional, Tuple
+from typing import Any, Callable, Dict, Optional, Tuple
 
 class FunctionResult:
     """The result of a BAML function call.
