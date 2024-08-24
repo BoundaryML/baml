a5d8c588e0fd0b7e186d7c71f1f6171334250629
gleed <aliigleed16@gmail.com>
2024-08-23 21:19:46 -0400

    feat: Update Default Gemini Base URL to v1beta (#891)
    In this PR, I updated the default base URL in the googleai_client.rs
    file to point to the beta version endpoint by changing
    "https://generativelanguage.googleapis.com/v1" to
    "https://generativelanguage.googleapis.com/v1beta".
    
    This mirrors the default implementation used in the [Google AI Python
    SDK](https://github.com/google-gemini/generative-ai-python/blob/8a29017e9120f0552ee3ad6092e8545d1aa6f803/google/generativeai/client.py#L60).
    
    Functionally this opens up a larger variety of models including gemini
    1.5 pro experimental from this month.
    
    v1:
    models/embedding-001
    models/gemini-1.0-pro
    models/gemini-1.0-pro-001
    models/gemini-1.0-pro-latest
    models/gemini-1.0-pro-vision-latest
    models/gemini-1.5-flash
    models/gemini-1.5-flash-001
    models/gemini-1.5-flash-001-tuning
    models/gemini-1.5-pro
    models/gemini-1.5-pro-001
    models/gemini-pro
    models/gemini-pro-vision
    models/text-embedding-004
    
    v1beta:
    models/aqa
    models/chat-bison-001
    models/embedding-001
    models/embedding-gecko-001
    models/gemini-1.0-pro
    models/gemini-1.0-pro-001
    models/gemini-1.0-pro-latest
    models/gemini-1.0-pro-vision-latest
    models/gemini-1.5-flash
    models/gemini-1.5-flash-001
    models/gemini-1.5-flash-001-tuning
    models/gemini-1.5-flash-latest
    models/gemini-1.5-pro
    models/gemini-1.5-pro-001
    models/gemini-1.5-pro-exp-0801
    models/gemini-1.5-pro-latest
    models/gemini-pro
    models/gemini-pro-vision
    models/text-bison-001
    models/text-embedding-004

--- engine/baml-runtime/src/internal/llm_client/primitive/google/googleai_client.rs
+++ engine/baml-runtime/src/internal/llm_client/primitive/google/googleai_client.rs
@@ -69,7 +69,7 @@ fn resolve_properties(
     let base_url = properties
         .remove("base_url")
         .and_then(|v| v.as_str().map(|s| s.to_string()))
-        .unwrap_or_else(|| "https://generativelanguage.googleapis.com/v1".to_string());
+        .unwrap_or_else(|| "https://generativelanguage.googleapis.com/v1beta".to_string());
     let allowed_metadata = match properties.remove("allowed_role_metadata") {
         Some(allowed_metadata) => serde_json::from_value(allowed_metadata)
             .context("allowed_role_metadata must be 'all', 'none', or ['key1', 'key2']")?,
