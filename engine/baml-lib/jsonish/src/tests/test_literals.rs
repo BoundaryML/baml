d04419ba1ace6cbfbe330d51d95e075d7dca8dc8
Antonio Sarosi <sarosiantonio@gmail.com>
2024-10-08 15:41:01 +0100

    Add more unit tests

--- engine/baml-lib/jsonish/src/tests/test_literals.rs
+++ engine/baml-lib/jsonish/src/tests/test_literals.rs
@@ -11,11 +11,19 @@ test_deserializer!(
 );
 
 test_deserializer!(
-    test_literal_boolean,
+    test_literal_boolean_true,
     EMPTY_FILE,
     "true",
     FieldType::Literal(LiteralValue::Bool(true)),
     true
+);
+
+test_deserializer!(
+    test_literal_boolean_false,
+    EMPTY_FILE,
+    "false",
+    FieldType::Literal(LiteralValue::Bool(false)),
+    false
 );
 
 test_deserializer!(
@@ -30,6 +38,38 @@ test_deserializer!(
     test_literal_string_uppercase_without_quotes,
     EMPTY_FILE,
     "TWO",
+    FieldType::Literal(LiteralValue::String("TWO".into())),
+    "TWO"
+);
+
+test_deserializer!(
+    test_literal_string_mismatched_case,
+    EMPTY_FILE,
+    "Two",
+    FieldType::Literal(LiteralValue::String("TWO".into())),
+    "TWO"
+);
+
+test_deserializer!(
+    test_literal_string_preceded_by_extra_text,
+    EMPTY_FILE,
+    "The answer is TWO",
+    FieldType::Literal(LiteralValue::String("TWO".into())),
+    "TWO"
+);
+
+test_deserializer!(
+    test_literal_string_followed_by_extra_text,
+    EMPTY_FILE,
+    "TWO is the answer",
+    FieldType::Literal(LiteralValue::String("TWO".into())),
+    "TWO"
+);
+
+test_deserializer!(
+    test_literal_string_with_quotes_preceded_by_extra_text,
+    EMPTY_FILE,
+    r#"The answer is "TWO""#,
     FieldType::Literal(LiteralValue::String("TWO".into())),
     "TWO"
 );
