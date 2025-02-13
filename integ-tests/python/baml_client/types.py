dcf765a02da1649821312a182af6ba230e425937
Chris Watts <chris@boundaryml.com>
2025-02-13 14:31:20 -0800

    chore: code gen

--- integ-tests/python/baml_client/types.py
+++ integ-tests/python/baml_client/types.py
@@ -558,6 +558,10 @@ class WithReasoning(BaseModel):
     value: str
     reasoning: str
 
+JsonEntry: TypeAlias = Union["SimpleTag", "JsonTemplate"]
+
+JsonTemplate: TypeAlias = Dict[str, "JsonEntry"]
+
 RecursiveMapAlias: TypeAlias = Dict[str, "RecursiveMapAlias"]
 
 RecursiveListAlias: TypeAlias = List["RecursiveListAlias"]
@@ -573,7 +577,3 @@ JsonValue: TypeAlias = Union[int, str, bool, float, "JsonObject", "JsonArray"]
 JsonObject: TypeAlias = Dict[str, "JsonValue"]
 
 JsonArray: TypeAlias = List["JsonValue"]
-
-JsonEntry: TypeAlias = Union["SimpleTag", "JsonTemplate"]
-
-JsonTemplate: TypeAlias = Dict[str, "JsonEntry"]
