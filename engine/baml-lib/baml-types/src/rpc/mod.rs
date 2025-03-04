1d10af3f5a12240a7703e338608280ba74aedcda
Sam Lijin <sam@boundaryml.com>
2025-03-03 17:17:45 -0800

    add types for the src upload handshake

--- engine/baml-lib/baml-types/src/rpc/mod.rs
+++ engine/baml-lib/baml-types/src/rpc/mod.rs
@@ -1,3 +1,4 @@
+pub mod upload_baml_src;
 use std::sync::Arc;
 
 use indexmap::IndexMap;
@@ -10,39 +11,6 @@ pub struct StudioTraceEventBatch {
     pub project_id: String,
     pub events: Vec<Arc<TraceEvent>>,
 }
-
-// ------------------------------------------------------------------------------------------------
-
-// TODO: version handling should be non-exhaustive for all of these
-// clients need to say "i can only handle v1 responses"
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum BamlSrcUploadStatus {
-    Pending,
-    Completed,
-    Failed,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "version")]
-pub enum GetBamlSrcUploadStatusRequest {
-    V1 {
-        project_id: String,
-        fingerprint: String,
-    },
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "version")]
-pub enum GetBamlSrcUploadStatusResponse {
-    V1 {
-        project_id: String,
-        fingerprint: String,
-        status: BamlSrcUploadStatus,
-    },
-}
-
-// ------------------------------------------------------------------------------------------------
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "version")]
---
+++ engine/baml-lib/baml-types/src/rpc/upload_baml_src.rs
@@ -0,0 +1,127 @@
+use serde::{Deserialize, Serialize};
+
+// TODO: version handling should be non-exhaustive for all of these
+// clients need to say "i can only handle v1 responses"
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BamlSrcUploadStatus {
+    Pending,
+    Completed,
+    Failed,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GetBamlSrcUploadStatusRequest {
+    pub project_id: String,
+    pub baml_src_id: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GetBamlSrcUploadStatusResponse {
+    pub project_id: String,
+    pub baml_src_id: String,
+    pub status: BamlSrcUploadStatus,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UploadBamlSrcRequest {
+    pub project_id: String,
+    pub baml_src_id: String,
+    pub function_definitions: Vec<BamlFunctionDefinition>,
+    pub type_definitions: Vec<BamlTypeDefinition>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UploadBamlSrcResponse {
+    pub project_id: String,
+    pub baml_src_id: String,
+}
+
+// ------------------------------------------------------------------------------------------------
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BamlTypeId(String);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum BamlTypeReference {
+    Null,
+    Int,
+    Bool,
+    Float,
+    String,
+    Class {
+        type_id: BamlTypeId,
+    },
+    Enum {
+        type_id: BamlTypeId,
+    },
+    TypeAlias {
+        type_id: BamlTypeId,
+    },
+    Array {
+        items: Box<BamlTypeReference>,
+    },
+    Map {
+        key: Box<BamlTypeReference>,
+        value: Box<BamlTypeReference>,
+    },
+    Union {
+        #[serde(rename = "anyOf")]
+        any_of: Vec<BamlTypeReference>,
+    },
+    Literal(BamlLiteralTypeReference),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "literal_type", content = "literal", rename_all = "snake_case")]
+pub enum BamlLiteralTypeReference {
+    String(String),
+    Int(i64),
+    Bool(bool),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum BamlTypeDefinition {
+    Class(BamlClassDefinition),
+    Enum(BamlEnumDefinition),
+    TypeAlias(BamlTypeAliasDefinition),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BamlClassDefinition {
+    pub type_id: BamlTypeId,
+    pub fields: Vec<BamlClassField>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BamlClassField {
+    pub name: String,
+    pub r#type: BamlTypeReference,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BamlEnumDefinition {
+    pub type_id: BamlTypeId,
+    pub values: Vec<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BamlTypeAliasDefinition {
+    pub type_id: BamlTypeId,
+    pub type_reference: BamlTypeReference,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BamlFunctionDefinition {
+    pub function_id: String,
+    pub inputs: Vec<BamlFunctionInput>,
+    pub output: BamlTypeReference,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BamlFunctionInput {
+    pub name: String,
+    pub value: BamlTypeReference,
+}
