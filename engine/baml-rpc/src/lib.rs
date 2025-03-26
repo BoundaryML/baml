cfccb83ec2d37984b25e10446915009354fac26d
Sam Lijin <sam@boundaryml.com>
2025-03-25 17:39:15 -0700

    more

--- engine/baml-rpc/src/lib.rs
+++ engine/baml-rpc/src/lib.rs
@@ -1,12 +1,14 @@
 mod ast;
 mod ast_node_id;
 mod baml_src_upload;
+mod base;
 mod define_id;
 mod rpc;
 mod trace;
 mod trace_event_upload;
 mod ui_control_plane_orgs;
 mod ui_control_plane_projects;
+mod ui_dashboard;
 mod ui_function_spans;
 
 pub use rpc::{ApiEndpoint, GetEndpoint};
