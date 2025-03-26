5342cd83c867e54a3fd9e80eb1cd1afd5285cc0a
Aaron Villalpando <aaron@boundaryml.com>
2025-03-26 13:38:12 -0700

    run integ tests

--- integ-tests/typescript/baml_client/index.ts
+++ integ-tests/typescript/baml_client/index.ts
@@ -18,11 +18,11 @@ $ pnpm add @boundaryml/baml
 /**
  * If this import fails, you may need to upgrade @boundaryml/baml.
  * 
- * Please upgrade @boundaryml/baml to 0.81.2.
+ * Please upgrade @boundaryml/baml to 0.81.3.
  * 
- * $ npm install @boundaryml/baml@0.81.2
- * $ yarn add @boundaryml/baml@0.81.2
- * $ pnpm add @boundaryml/baml@0.81.2
+ * $ npm install @boundaryml/baml@0.81.3
+ * $ yarn add @boundaryml/baml@0.81.3
+ * $ pnpm add @boundaryml/baml@0.81.3
  * 
  * If nothing else works, please ask for help:
  * 
@@ -32,7 +32,7 @@ $ pnpm add @boundaryml/baml
  **/
 import { ThrowIfVersionMismatch } from "@boundaryml/baml";
 
-export const version = "0.81.2";
+export const version = "0.81.3";
 
 ThrowIfVersionMismatch(version);
 
