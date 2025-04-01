a834651e91035cb46326c6f30866dab4c4772a8b
Greg Hale <imalsogreg@gmail.com>
2025-03-31 20:11:43 -0700

    More random changes for ruby

--- engine/language_client_ruby/arm-linux-setup.sh
+++ engine/language_client_ruby/arm-linux-setup.sh
@@ -3,4 +3,8 @@
 set -euxo pipefail
 
 sudo apt-get update
-sudo apt-get install -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+sudo apt-get install -y \
+  gcc-arm-linux-gnueabihf \
+  g++-arm-linux-gnueabihf \
+  gcc-aarch64-linux-gnu \
+  g++-aarch64-linux-gnu
