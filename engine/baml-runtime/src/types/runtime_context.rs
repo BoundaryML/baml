a84aa3bed03454939d138b2474626ebd17e56372
Sam Lijin <sam@boundaryml.com>
2025-04-05 01:14:58 -0700

    one more fix

--- engine/baml-runtime/src/types/runtime_context.rs
+++ engine/baml-runtime/src/types/runtime_context.rs
@@ -87,10 +87,10 @@ cfg_if::cfg_if!(
     if #[cfg(target_arch = "wasm32")] {
         use core::pin::Pin;
         use core::future::Future;
-        pub type BamlSrcReader = Option<Box<dyn Fn(&str) -> core::pin::Pin<Box<dyn Future<Output = Result<Vec<u8>>>>>>>;
+        pub type BamlSrcReader = Option<Box<dyn Fn(&str) -> core::pin::Pin<Box<dyn Future<Output = RuntimeCallbackResult<Vec<u8>>>>>>>;
     } else {
         use futures::future::BoxFuture;
-        pub type BamlSrcReader = Option<Box<fn(&str) -> BoxFuture<'static, Result<Vec<u8>>>>>;
+        pub type BamlSrcReader = Option<Box<fn(&str) -> BoxFuture<'static, RuntimeCallbackResult<Vec<u8>>>>>;
     }
 );
 
