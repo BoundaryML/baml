2b5dd0355f14beb98c8a66c23f9ad0f856bbc807
Antonio Sarosi <sarosiantonio@gmail.com>
2025-04-09 18:06:39 +0200

    Error message

--- typescript/vscode-ext/packages/vscode/src/plugins/language-server/index.ts
+++ typescript/vscode-ext/packages/vscode/src/plugins/language-server/index.ts
@@ -436,17 +436,21 @@ const plugin: BamlVSCodePlugin = {
 
     // TODO: Send notification, loading state, etc.
     if (!(await checkIfCliBinaryExists(cliVersion))) {
-      await window.withProgress(
+      window.withProgress(
         {
           location: vscode.ProgressLocation.Notification,
           cancellable: false,
           title: 'Downloading BAML LSP',
         },
         async (progress, token) => {
-          await downloadCli(cliVersion)
+          try {
+            await downloadCli(cliVersion)
+            window.showInformationMessage(`BAML LSP v${cliVersion.version} downloaded!`)
+          } catch (error) {
+            window.showErrorMessage(`Failed to download BAML LSP: ${error}`)
+          }
         },
       )
-      window.showInformationMessage(`BAML LSP v${cliVersion.version} downloaded!`)
     }
 
     activateClient(context, serverOptions, clientOptions)
