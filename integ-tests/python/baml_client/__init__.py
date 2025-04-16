c7f0151ecfc771f42669bcc496c675f892aaee45
Aaron Villalpando <aaron@boundaryml.com>
2025-04-16 10:53:11 -0700

    Run integ tests for 0.84.1

--- integ-tests/python/baml_client/__init__.py
+++ integ-tests/python/baml_client/__init__.py
@@ -13,7 +13,7 @@
 # flake8: noqa: E501,F401
 # pylint: disable=unused-import,line-too-long
 # fmt: off
-__version__ = "0.84.0"
+__version__ = "0.84.1"
 
 try:
   from baml_py.safe_import import EnsureBamlPyImport
