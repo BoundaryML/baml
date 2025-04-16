1510a3d50df6780e508d5b57d5f9b9b253df3ad0
Aaron Villalpando <aaron@boundaryml.com>
2025-04-16 11:47:24 -0700

    Bump version to 0.84.2

--- engine/language_client_go/baml_go/lib.go
+++ engine/language_client_go/baml_go/lib.go
@@ -34,7 +34,7 @@ import (
 import "C"
 
 const (
-	VERSION            = "0.84.1"
+	VERSION            = "0.84.2"
 	githubRepo         = "boundaryml/baml"
 	bamlCacheDirEnvVar = "BAML_CACHE_DIR"
 	bamlLibraryPathEnv = "BAML_LIBRARY_PATH"
