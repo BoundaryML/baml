115ed8dfc57e1399af22f071a0c43162bf1b479b
Sam Lijin <sam@boundaryml.com>
2025-04-17 13:39:11 -0700

    Bump version to 0.84.4

--- engine/language_client_go/baml_go/lib.go
+++ engine/language_client_go/baml_go/lib.go
@@ -34,7 +34,7 @@ import (
 import "C"
 
 const (
-	VERSION            = "0.84.3"
+	VERSION            = "0.84.4"
 	githubRepo         = "boundaryml/baml"
 	bamlCacheDirEnvVar = "BAML_CACHE_DIR"
 	bamlLibraryPathEnv = "BAML_LIBRARY_PATH"
