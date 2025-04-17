bb7240cda06d344674e9a7cb7fb390020c48507a
Dimitri Roche <635121+dimroc@users.noreply.github.com>
2025-04-17 11:22:34 -0700

    ruby: remove user path from runtime_test.rb

--- engine/language_client_ruby/test/runtime_test.rb
+++ engine/language_client_ruby/test/runtime_test.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-require_relative "../lib/baml"
+require_relative '../lib/baml'
 
 require 'minitest/autorun'
 require 'minitest/reporters'
 
-describe "runtime sanity check" do
-  it "can build runtime" do
-    baml = Baml::Ffi::BamlRuntime.from_directory("/Users/sam/baml/integ-tests/baml_src", {})
+describe 'runtime sanity check' do
+  it 'can build runtime' do
+    # Construct the path relative to the current file's directory (__dir__)
+    # Go up three levels to the project root, then into integ-tests/baml_src
+    baml_src_dir = File.expand_path('../../../integ-tests/baml_src', __dir__)
+    Baml::Ffi::BamlRuntime.from_directory(baml_src_dir, {})
     # assert_equal(baml.always_error("input"), "0.1.0")
-
   end
 end
 
