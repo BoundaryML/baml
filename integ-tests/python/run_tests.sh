ab1aff428e06c9ac5a37867aea2fcbaa92719a74
Aaron Villalpando <aaron@boundaryml.com>
2025-05-07 11:34:36 -0700

    filter test

--- integ-tests/python/run_tests.sh
+++ integ-tests/python/run_tests.sh
@@ -9,6 +9,6 @@ uv run maturin develop --uv --manifest-path ../../engine/language_client_python/
 uv run baml-cli generate --from ../baml_src
 
 # These tests are excluded because they require credentials.
-uv run pytest "$@" --ignore=tests/test_functions.py --ignore=tests/test_collector.py --ignore=tests/test_with_options.py --ignore=tests/test_modular_api.py --ignore=tests/test_logger.py
+uv run pytest "$@" --ignore=tests/test_functions.py --ignore=tests/test_collector.py --ignore=tests/test_with_options.py --ignore=tests/test_modular_api.py --ignore=tests/test_logger.py --ignore=tests/test_typebuilder.py
 
 uv run ruff check .
