41d6d2278b5fef2304b0d593fde2b84704d0425f
Sam Lijin <sam@boundaryml.com>
2025-05-23 18:10:24 -0700

    fix workflwos

--- jetbrains/build.gradle.kts
+++ jetbrains/build.gradle.kts
@@ -94,7 +94,7 @@ intellijPlatform {
     }
 
     publishing {
-        token = providers.environmentVariable("PUBLISH_TOKEN")
+        token = providers.environmentVariable("INTELLIJ_PLATFORM_PUBLISH_TOKEN")
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
