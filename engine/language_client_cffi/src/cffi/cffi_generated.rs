9574b86c6887e59d425002ed57a4efe8eb5606c1
Rahul Tiwari <jprrahultiwari@gmail.com>
2025-06-20 11:47:29 -0700

    run cargo fix

--- engine/language_client_cffi/src/cffi/cffi_generated.rs
+++ engine/language_client_cffi/src/cffi/cffi_generated.rs
@@ -3,11 +3,8 @@
 
 // @generated
 
-use core::mem;
-use core::cmp::Ordering;
 
 extern crate flatbuffers;
-use self::flatbuffers::{EndianScalar, Follow};
 
 #[allow(unused_imports, dead_code)]
 pub mod cffi {
