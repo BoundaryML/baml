4e7fcd10c917b8aa94138bae485b77dbbb2e8112
Aaron Villalpando <aaron@boundaryml.com>
2025-07-11 15:01:35 -0700

    fix issue

--- engine/baml-runtime/src/tracingv2/publisher/rpc_converters/trace_data.rs
+++ engine/baml-runtime/src/tracingv2/publisher/rpc_converters/trace_data.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 
 use anyhow::Result;
-use baml_rpc::ClientDetails;
+use baml_rpc::RpcClientDetails;
 use baml_types::{tracing::events::FunctionType, type_meta, HasType};
 
 use super::{IntoRpcEvent, TypeLookup};
@@ -231,7 +231,7 @@ impl<'a> IntoRpcEvent<'a, baml_rpc::runtime_api::IntermediateData<'a>>
             status: self.status,
             headers: self.headers().cloned(),
             body: self.body.to_rpc_event(lookup),
-            client_details: ClientDetails {
+            client_details: RpcClientDetails {
                 name: self.client_details.name.clone(),
                 provider: self.client_details.provider.clone(),
                 options: self.client_details.options.clone(),
