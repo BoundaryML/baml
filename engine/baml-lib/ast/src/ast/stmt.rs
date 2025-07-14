e73c130448b9c412790403bcabd59001f147e6ab
Antonio Sarosi <sarosiantonio@gmail.com>
2025-07-14 21:28:00 +0200

    Get rid of long tuples

--- engine/baml-lib/ast/src/ast/stmt.rs
+++ engine/baml-lib/ast/src/ast/stmt.rs
@@ -2,20 +2,33 @@ use std::fmt;
 
 use super::{Expression, ExpressionBlock, Identifier, Span};
 
+#[derive(Debug, Clone)]
+pub struct LetStmt {
+    pub identifier: Identifier,
+    pub expr: Expression,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub struct ForLoopStmt {
+    pub identifier: Identifier,
+    pub iterator: Expression,
+    pub body: ExpressionBlock,
+    pub span: Span,
+}
+
 // Stmt(statements) perform actions and not often return values.
 #[derive(Debug, Clone)]
 pub enum Stmt {
-    Let(Identifier, Expression, Span),
-    ForLoop(Identifier, Expression, ExpressionBlock, Span),
+    Let(LetStmt),
+    ForLoop(ForLoopStmt),
 }
 
 impl fmt::Display for Stmt {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Stmt::Let(identifier, expr, span) => write!(f, "let {} = {}", identifier, expr)?,
-            Stmt::ForLoop(identifier, expr, block, span) => {
-                write!(f, "for {} in {}", identifier, expr)?
-            }
+            Stmt::Let(stmt) => write!(f, "let {} = {}", stmt.identifier, stmt.expr)?,
+            Stmt::ForLoop(stmt) => write!(f, "for {} in {}", stmt.identifier, stmt.iterator)?,
         }
         Ok(())
     }
@@ -24,22 +37,19 @@ impl fmt::Display for Stmt {
 impl Stmt {
     pub fn assert_eq_up_to_span(&self, other: &Stmt) {
         match (self, other) {
-            (Stmt::Let(identifier1, expr1, span1), Stmt::Let(identifier2, expr2, span2)) => {
-                identifier1.assert_eq_up_to_span(identifier2);
-                expr1.assert_eq_up_to_span(expr2);
+            (Stmt::Let(stmt1), Stmt::Let(stmt2)) => {
+                stmt1.identifier.assert_eq_up_to_span(&stmt2.identifier);
+                stmt1.expr.assert_eq_up_to_span(&stmt2.expr);
             }
-            (
-                Stmt::ForLoop(identifier1, expr1, block1, span1),
-                Stmt::ForLoop(identifier2, expr2, block2, span2),
-            ) => {
-                identifier1.assert_eq_up_to_span(identifier2);
-                expr1.assert_eq_up_to_span(expr2);
-                block1.assert_eq_up_to_span(block2);
+            (Stmt::ForLoop(stmt1), Stmt::ForLoop(stmt2)) => {
+                stmt1.identifier.assert_eq_up_to_span(&stmt2.identifier);
+                stmt1.iterator.assert_eq_up_to_span(&stmt2.iterator);
+                stmt1.body.assert_eq_up_to_span(&stmt2.body);
             }
-            (Stmt::Let(_, _, _), Stmt::ForLoop(_, _, _, _)) => {
+            (Stmt::Let(_), Stmt::ForLoop(_)) => {
                 panic!("Types do not match: {self:?} and {other:?}")
             }
-            (Stmt::ForLoop(_, _, _, _), Stmt::Let(_, _, _)) => {
+            (Stmt::ForLoop(_), Stmt::Let(_)) => {
                 panic!("Types do not match: {self:?} and {other:?}")
             }
         }
@@ -47,22 +57,22 @@ impl Stmt {
 
     pub fn identifier(&self) -> &Identifier {
         match self {
-            Stmt::Let(identifier, _, _) => identifier,
-            Stmt::ForLoop(identifier, _, _, _) => identifier,
+            Stmt::Let(stmt) => &stmt.identifier,
+            Stmt::ForLoop(stmt) => &stmt.identifier,
         }
     }
 
     pub fn span(&self) -> &Span {
         match self {
-            Stmt::Let(_, _, span) => span,
-            Stmt::ForLoop(_, _, _, span) => span,
+            Stmt::Let(stmt) => &stmt.span,
+            Stmt::ForLoop(stmt) => &stmt.span,
         }
     }
 
     pub fn body(&self) -> &Expression {
         match self {
-            Stmt::Let(_, expr, _) => expr,
-            Stmt::ForLoop(_, _, block, _) => &block.expr,
+            Stmt::Let(stmt) => &stmt.expr,
+            Stmt::ForLoop(stmt) => &stmt.body.expr,
         }
     }
 }
