17952e146ed1a768289ef87e91f48ba22a1248f1
Chris Watts <chris@boundaryml.com>
2025-07-29 16:46:36 -0700

    fix: issue with cps running vscode in dev

--- typescript/apps/vscode-ext/src/panels/WebviewPanelHost.ts
+++ typescript/apps/vscode-ext/src/panels/WebviewPanelHost.ts
@@ -257,7 +257,6 @@ export class WebviewPanelHost {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="Content-Security-Policy" content="${csp.join('; ')}">
         <link rel="stylesheet" type="text/css" href="${stylesUri}">
         <title>BAML Playground</title>
         <style>
