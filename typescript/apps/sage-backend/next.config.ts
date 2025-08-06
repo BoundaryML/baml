a4398bfa98febbfcc874335bc68066fcb4efdf25
Sam Lijin <sam@boundaryml.com>
2025-08-06 12:59:07 -0700

    lets see if this works

--- typescript/apps/sage-backend/next.config.ts
+++ typescript/apps/sage-backend/next.config.ts
@@ -4,6 +4,7 @@ import { withBaml } from '@boundaryml/baml-nextjs-plugin';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  transpilePackages: ['@baml/sage-interface'],
 };
 
 export default withBaml()(nextConfig);
