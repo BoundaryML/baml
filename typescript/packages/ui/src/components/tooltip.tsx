87550a605c1a3d3af3497d92b82f6f785b4100fe
Aaron Villalpando <aaron@boundaryml.com>
2025-08-05 21:11:42 -0700

    Fix tooltip

--- typescript/packages/ui/src/components/tooltip.tsx
+++ typescript/packages/ui/src/components/tooltip.tsx
@@ -52,7 +52,7 @@ function TooltipContent({
         {...props}
       >
         {children}
-        <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-secondary fill-secondary" />
+        <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-sidebar fill-sidebar" />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   );
