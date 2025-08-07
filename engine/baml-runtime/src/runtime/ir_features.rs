1562f060b73ae7a8c991335d1e97c8c3de38fb8b
Antonio Sarosi <sarosiantonio@gmail.com>
2025-08-07 17:59:07 +0200

    More lifetimes

--- engine/baml-runtime/src/runtime/ir_features.rs
+++ engine/baml-runtime/src/runtime/ir_features.rs
@@ -13,11 +13,11 @@ impl WithInternal for InternalBamlRuntime {
         IrFeatures::from(vec![], ir.walk_functions().any(|f| f.is_v2()), vec![])
     }
 
-    fn walk_functions(&self) -> impl ExactSizeIterator<Item = FunctionWalker> {
+    fn walk_functions(&self) -> impl ExactSizeIterator<Item = FunctionWalker<'_>> {
         self.ir().walk_functions()
     }
 
-    fn walk_tests(&self) -> impl Iterator<Item = TestCaseWalker> {
+    fn walk_tests(&self) -> impl Iterator<Item = TestCaseWalker<'_>> {
         self.ir().walk_tests()
     }
 }
