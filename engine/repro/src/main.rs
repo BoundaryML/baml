bea010e3e82ccc0daab34fc1b5d4f91e74661304
Sam Lijin <sam@boundaryml.com>
2025-08-20 20:39:58 -0700

    finally fkn repro the crash

--- engine/repro/src/main.rs
+++ engine/repro/src/main.rs
@@ -1,7 +1,12 @@
 use repro::run_server;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from("repro=debug,info"))
+        .init();
+
     run_server().await?;
     Ok(())
 }
