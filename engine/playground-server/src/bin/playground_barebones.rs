576e46c2cb2eeb00517190651fa080b5ee3675a4
Sam Lijin <sam@boundaryml.com>
2025-08-30 16:20:15 -0700

    cargo fmt

--- engine/playground-server/src/bin/playground_barebones.rs
+++ engine/playground-server/src/bin/playground_barebones.rs
@@ -1,6 +1,4 @@
-use std::collections::HashMap;
-use std::fs;
-use std::path::Path;
+use std::{collections::HashMap, fs, path::Path};
 
 use playground_server::{
     pick_ports, AppState, FrontendMessage, LangServerToWasmMessage, PlaygroundServer,
