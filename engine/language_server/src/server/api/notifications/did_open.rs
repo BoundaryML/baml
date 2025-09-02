4044e1741b60df702c6656f8fa4d44b0b61162fa
Jesus Lapastora Núñez <jesus@boundaryml.com>
2025-09-02 15:15:54 +0200

    cargo fmt

--- engine/language_server/src/server/api/notifications/did_open.rs
+++ engine/language_server/src/server/api/notifications/did_open.rs
@@ -84,7 +84,6 @@ impl SyncNotificationHandler for DidOpenTextDocumentHandler {
 
             let generator_version = locked.get_common_generator_version();
             send_generator_version(&notifier, &*locked, generator_version.as_ref().ok());
-
         } else {
             tracing::error!("Failed to get or create project for path: {:?}", file_path);
             show_err_msg!("Failed to get or create project for path: {:?}", file_path);
