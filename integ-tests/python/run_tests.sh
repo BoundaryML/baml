a4edcbcc225ead86b6f14937a646ea114bb4e504
Sam Lijin <sam@boundaryml.com>
2025-09-15 11:21:05 -0700

    fix run_tests

--- integ-tests/python/run_tests.sh
+++ integ-tests/python/run_tests.sh
@@ -11,6 +11,7 @@ uv run baml-cli generate --from ../baml_src
 # These tests are excluded because they require credentials.
 uv run pytest "$@" \
     --ignore=tests/test_functions.py \
+    --ignore=tests/test_errors.py \
     --ignore=tests/test_collector.py \
     --ignore=tests/test_with_options.py \
     --ignore=tests/test_modular_api.py \
