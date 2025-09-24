19bc2b2e4b04b5c1c650a6de6f4f989b121bea3c
Aaron Villalpando <aaron@boundaryml.com>
2025-09-24 14:03:37 -0700

    Bump version to 0.208.4

--- engine/language_client_go/baml_go/lib.go
+++ engine/language_client_go/baml_go/lib.go
@@ -33,7 +33,7 @@ import (
 import "C"
 
 const (
-	VERSION            = "0.208.3"
+	VERSION            = "0.208.4"
 	githubRepo         = "boundaryml/baml"
 	bamlCacheDirEnvVar = "BAML_CACHE_DIR"
 	bamlLibraryPathEnv = "BAML_LIBRARY_PATH"
--- engine/language_client_ruby/baml.gemspec
+++ engine/language_client_ruby/baml.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |spec|
   spec.name = "baml"
-  spec.version = "0.208.3"
+  spec.version = "0.208.4"
   spec.authors = ["BoundaryML"]
   spec.email = ["contact@boundaryml.com"]
 
