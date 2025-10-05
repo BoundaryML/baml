46fd6b3a4755be5de0c7d6d1048251a223e86ec4
Antonio Sarosi <sarosiantonio@gmail.com>
2025-10-05 21:29:52 +0200

    Fix version changes

--- engine/generators/languages/python/generated_tests/primitive_types/baml_client/__init__.py
+++ engine/generators/languages/python/generated_tests/primitive_types/baml_client/__init__.py
@@ -10,7 +10,7 @@
 # BAML files and re-generate this code using: baml-cli generate
 # baml-cli is available with the baml package.
 
-__version__ = "0.209.0"
+__version__ = "0.208.5"
 
 try:
   from baml_py.safe_import import EnsureBamlPyImport
@@ -38,9 +38,9 @@ with EnsureBamlPyImport(__version__) as e:
   from . import stream_types
   from . import config
   from .config import reset_baml_env_vars
-  
+
   from .async_client import b
-  
+
 
 
 # FOR LEGACY COMPATIBILITY, expose "partial_types" as an alias for "stream_types"
--- engine/generators/languages/typescript/generated_tests/union_types_extended/baml_client/index.ts
+++ engine/generators/languages/typescript/generated_tests/union_types_extended/baml_client/index.ts
@@ -20,22 +20,22 @@ $ pnpm add @boundaryml/baml
 
 /**
  * If this import fails, you may need to upgrade @boundaryml/baml.
- * 
- * Please upgrade @boundaryml/baml to 0.209.0.
- * 
- * $ npm install @boundaryml/baml@0.209.0
- * $ yarn add @boundaryml/baml@0.209.0
- * $ pnpm add @boundaryml/baml@0.209.0
- * 
+ *
+ * Please upgrade @boundaryml/baml to 0.208.5.
+ *
+ * $ npm install @boundaryml/baml@0.208.5
+ * $ yarn add @boundaryml/baml@0.208.5
+ * $ pnpm add @boundaryml/baml@0.208.5
+ *
  * If nothing else works, please ask for help:
- * 
+ *
  * https://github.com/boundaryml/baml/issues
  * https://boundaryml.com/discord
- * 
+ *
  **/
 import { ThrowIfVersionMismatch } from "@boundaryml/baml";
 
-export const version = "0.209.0";
+export const version = "0.208.5";
 
 ThrowIfVersionMismatch(version);
 
