c47220f307325b155c556a61ec4ca2ad31bb2fdc
Greg Hale <imalsogreg@gmail.com>
2025-10-06 19:21:03 -0700

    override env var test when a proxy server is available

--- engine/baml-runtime/src/runtime/runtime_interface.rs
+++ engine/baml-runtime/src/runtime/runtime_interface.rs
@@ -109,6 +109,8 @@ impl<'a> InternalClientLookup<'a> for InternalBamlRuntime {
                 // Collect required environment variables.
                 let mut required_env_vars = HashMap::new();
 
+                let uses_proxy_server = ctx.proxy_url().is_some();
+
                 // If the client is Vertex or AWS Bedrock, we don't fail on
                 // missing required environment variables because we run a bunch
                 // of additional logic to resolve required values (i.e for
@@ -117,7 +119,11 @@ impl<'a> InternalClientLookup<'a> for InternalBamlRuntime {
                 //
                 // If called with modular API, we don't fail either. User might
                 // want to insert the values later.
+                //
+                // When a proxy server is used, don't fail on missing required
+                // env vars because the proxy server is likely to provide them.
                 let fail_on_missing_required_env_vars = !ctx.is_modular_api()
+                    && !uses_proxy_server
                     && !matches!(
                         walker.item.elem.provider,
                         ClientProvider::AwsBedrock | ClientProvider::Vertex
