eb820a41cd3e8b28e2ac5c3b67ba186faec5fb90
Aaron Villalpando <aaron@boundaryml.com>
2025-10-22 20:37:06 -0700

    improve error name

--- engine/baml-runtime/src/errors.rs
+++ engine/baml-runtime/src/errors.rs
@@ -163,7 +163,7 @@ impl IntoBamlError for &anyhow::Error {
             return baml_error.clone();
         }
         baml_types::tracing::events::BamlError::External {
-            message: Cow::Owned(format!("into_baml_error: {self:?}")),
+            message: Cow::Owned(format!("ExternalException: {self:?}")),
         }
     }
 }
