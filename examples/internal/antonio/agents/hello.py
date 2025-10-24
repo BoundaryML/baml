aa2278b83a6cf12a8af21dc039fa43876aedee4b
Antonio Sarosi <sarosiantonio@gmail.com>
2025-10-24 18:22:08 +0200

    Planning activities agent

--- examples/internal/antonio/agents/hello.py
+++ examples/internal/antonio/agents/hello.py
@@ -2,7 +2,7 @@ from baml_client import b
 
 
 def stock_agent():
-    prompt = input("Prompt: ")
+    prompt = input("Ask LLM about stock price of a company: ")
     response = b.StockAgent(prompt)
     print(response)
 
@@ -17,9 +17,24 @@ def human_loop_agent():
     response = b.GuessGameAgent()
     print(response)
 
+def planning_agent():
+    user_input = input("Where do you want to plan activities?: ")
+    response = b.ActivityPlanningWorkflow(user_input)
+    print(response)
+
 
 def main():
-    human_loop_agent()
+    agent = input("Which agent do you want to run? (stock_agent, recruiter_agent, human_loop_agent, planning_agent): ")
+    if agent == "stock_agent":
+        stock_agent()
+    elif agent == "recruiter_agent":
+        recruiter_agent()
+    elif agent == "human_loop_agent":
+        human_loop_agent()
+    elif agent == "planning_agent":
+        planning_agent()
+    else:
+        print("Invalid agent")
 
 
 if __name__ == "__main__":
