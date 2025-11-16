4e419decd1989570c69c2fe4e3649a76c01406a1
Antonio Sarosi <sarosiantonio@gmail.com>
2025-11-16 19:48:17 +0100

    Restore code deleted by AI (lol)

--- engine/baml-lib/baml-core/src/validate/validation_pipeline/validations/classes.rs
+++ engine/baml-lib/baml-core/src/validate/validation_pipeline/validations/classes.rs
@@ -40,7 +40,7 @@ pub(super) fn validate(ctx: &mut Context<'_>) {
 
         for args in cls.walk_input_args() {
             let arg = args.ast_arg();
-            validate_type(ctx, &arg.1.field_type);
+            validate_type(ctx, &arg.1.field_type)
         }
 
         defined_types.start_scope();
