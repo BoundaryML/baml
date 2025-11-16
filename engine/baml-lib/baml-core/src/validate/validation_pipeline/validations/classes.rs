431172271ad8ded45c1d1ceab7651ac82efb42a3
Antonio Sarosi <sarosiantonio@gmail.com>
2025-11-16 19:44:08 +0100

    Support `@skip` attribute on class fields

--- engine/baml-lib/baml-core/src/validate/validation_pipeline/validations/classes.rs
+++ engine/baml-lib/baml-core/src/validate/validation_pipeline/validations/classes.rs
@@ -26,12 +26,16 @@ pub(super) fn validate(ctx: &mut Context<'_>) {
             let field = c.ast_field();
             if let Some(ft) = &field.expr {
                 validate_type(ctx, ft);
-            }
-        }
 
-        for args in cls.walk_input_args() {
-            let arg = args.ast_arg();
-            validate_type(ctx, &arg.1.field_type)
+                if let Some(skip) = field.attributes.iter().find(|a| a.name.name() == "skip") {
+                    if !ft.is_optional() {
+                        ctx.push_error(DatamodelError::new_validation_error(
+                            &format!("Class field with @skip attribute must be optional. Try making the type nullable: {} {}", field.name(), ft.to_nullable()),
+                            field.span().clone(),
+                        ));
+                    }
+                }
+            }
         }
 
         defined_types.start_scope();
