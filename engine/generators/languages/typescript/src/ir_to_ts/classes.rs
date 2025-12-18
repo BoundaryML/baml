a90883c6026e516e08f4a1d689a0caf75ff65fb7
hellovai <vbv@boundaryml.com>
2025-12-17 16:21:51 -0800

    fmt fixes

--- engine/generators/languages/typescript/src/ir_to_ts/classes.rs
+++ engine/generators/languages/typescript/src/ir_to_ts/classes.rs
@@ -76,9 +76,8 @@ fn ir_field_to_ts_stream<'a>(field: &Field, pkg: &'a CurrentRenderPackage) -> Fi
 mod tests {
     use internal_baml_core::ir::{repr::make_test_ir, IRHelper};
 
-    use crate::r#type::TypeTS;
-
     use super::*;
+    use crate::r#type::TypeTS;
 
     #[test]
     fn test_ir_class_to_ts() {
