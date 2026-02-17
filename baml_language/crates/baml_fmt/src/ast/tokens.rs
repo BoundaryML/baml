d1d95041425967971aa95433f04fdaf96c64eafe
2kai2kai2 <22033723+2kai2kai2@users.noreply.github.com>
2026-02-17 14:06:18 -0800

    baml_fmt: raw string formatting

--- baml_language/crates/baml_fmt/src/ast/tokens.rs
+++ baml_language/crates/baml_fmt/src/ast/tokens.rs
@@ -342,6 +342,50 @@ impl FromCST for BinaryOp {
     }
 }
 
+impl Printable for BinaryOp {
+    fn print(&self, _shape: Shape, printer: &mut Printer) -> PrintInfo {
+        match self {
+            BinaryOp::EqualsEquals(t) => printer.print_raw_token(t),
+            BinaryOp::NotEquals(t) => printer.print_raw_token(t),
+            BinaryOp::Less(t) => printer.print_raw_token(t),
+            BinaryOp::Greater(t) => printer.print_raw_token(t),
+            BinaryOp::LessEquals(t) => printer.print_raw_token(t),
+            BinaryOp::GreaterEquals(t) => printer.print_raw_token(t),
+            BinaryOp::AndAnd(t) => printer.print_raw_token(t),
+            BinaryOp::OrOr(t) => printer.print_raw_token(t),
+            BinaryOp::And(t) => printer.print_raw_token(t),
+            BinaryOp::Pipe(t) => printer.print_raw_token(t),
+            BinaryOp::Caret(t) => printer.print_raw_token(t),
+            BinaryOp::Instanceof(t) => printer.print_raw_token(t),
+            BinaryOp::LessLess(t) => printer.print_raw_token(t),
+            BinaryOp::GreaterGreater(t) => printer.print_raw_token(t),
+            BinaryOp::Plus(t) => printer.print_raw_token(t),
+            BinaryOp::Minus(t) => printer.print_raw_token(t),
+            BinaryOp::Star(t) => printer.print_raw_token(t),
+            BinaryOp::Slash(t) => printer.print_raw_token(t),
+            BinaryOp::Percent(t) => printer.print_raw_token(t),
+            BinaryOp::Equals(t) => printer.print_raw_token(t),
+            BinaryOp::PlusEquals(t) => printer.print_raw_token(t),
+            BinaryOp::MinusEquals(t) => printer.print_raw_token(t),
+            BinaryOp::StarEquals(t) => printer.print_raw_token(t),
+            BinaryOp::SlashEquals(t) => printer.print_raw_token(t),
+            BinaryOp::PercentEquals(t) => printer.print_raw_token(t),
+            BinaryOp::AndEquals(t) => printer.print_raw_token(t),
+            BinaryOp::PipeEquals(t) => printer.print_raw_token(t),
+            BinaryOp::CaretEquals(t) => printer.print_raw_token(t),
+            BinaryOp::LessLessEquals(t) => printer.print_raw_token(t),
+            BinaryOp::GreaterGreaterEquals(t) => printer.print_raw_token(t),
+        }
+        PrintInfo::default_single_line()
+    }
+    fn leftmost_token(&self) -> TextRange {
+        self.span()
+    }
+    fn rightmost_token(&self) -> TextRange {
+        self.span()
+    }
+}
+
 #[derive(Debug)]
 pub enum UnaryOp {
     Not(Not),
@@ -373,6 +417,22 @@ impl Token for UnaryOp {
             UnaryOp::Not(t) => t.span(),
             UnaryOp::Minus(t) => t.span(),
         }
+    }
+}
+
+impl Printable for UnaryOp {
+    fn print(&self, _shape: Shape, printer: &mut Printer) -> PrintInfo {
+        match self {
+            UnaryOp::Not(t) => printer.print_raw_token(t),
+            UnaryOp::Minus(t) => printer.print_raw_token(t),
+        }
+        PrintInfo::default_single_line()
+    }
+    fn leftmost_token(&self) -> TextRange {
+        self.span()
+    }
+    fn rightmost_token(&self) -> TextRange {
+        self.span()
     }
 }
 
@@ -547,10 +607,74 @@ impl KnownKind for RawString {
     }
 }
 impl Printable for RawString {
-    fn print(&self, _shape: Shape, printer: &mut Printer) -> PrintInfo {
+    fn print(&self, shape: Shape, printer: &mut Printer) -> PrintInfo {
         let text = &printer.input[self.span()];
         let multi_lined = text.contains('\n');
-        printer.print_raw_token(self);
+        if !multi_lined {
+            // print as-is
+            printer.print_raw_token(self);
+            return PrintInfo { multi_lined };
+        }
+
+        // we need to re-organize the interior
+        let (Some(start_quote), Some(end_quote)) = (text.find('"'), text.rfind('"')) else {
+            // should never happen, but print as-is if it does
+            printer.print_raw_token(self);
+            return PrintInfo { multi_lined };
+        };
+        if end_quote <= start_quote {
+            // should never happen, but print as-is if it does
+            printer.print_raw_token(self);
+            return PrintInfo { multi_lined };
+        }
+
+        let interior = &text[start_quote + 1..end_quote].trim();
+        let mut lines = interior.lines();
+        let first_line = lines
+            .next()
+            .unwrap_or_else(|| unreachable!("split always has at least one element"));
+        let min_indent = lines
+            .clone()
+            .map(|line| {
+                let count = line.bytes().take_while(|c| *c == b' ').count();
+                if count == line.len() {
+                    // it is all spaces
+                    usize::MAX
+                } else {
+                    count
+                }
+            })
+            .min()
+            .unwrap_or(0);
+
+        let inner_base_indent = shape.indent + printer.config.indent_width;
+        printer.print_str(&text[..start_quote + 1]);
+        printer.print_newline();
+        printer.print_spaces(inner_base_indent);
+        printer.print_str(first_line.trim_start_matches(' '));
+        for line in lines {
+            if line.len() <= min_indent {
+                // This line must be all spaces since otherwise it would have affected `min_indent`.
+                // So we can print an empty line.
+                printer.print_newline();
+                continue;
+            }
+
+            let (removed_indent, line) = line.split_at(min_indent);
+            debug_assert!(
+                removed_indent.bytes().all(|c| c == b' '),
+                "should not have removed non-indent"
+            );
+            debug_assert!(!line.is_empty(), "should have been handled above");
+
+            printer.print_newline();
+            printer.print_spaces(inner_base_indent);
+            printer.print_str(line);
+        }
+        printer.print_newline();
+        printer.print_spaces(shape.indent);
+        printer.print_str(&text[end_quote..]);
+
         PrintInfo { multi_lined }
     }
     fn leftmost_token(&self) -> TextRange {
@@ -564,66 +688,6 @@ impl Printable for RawString {
             self.token_span.end() - TextSize::from(1),
             self.token_span.end(),
         )
-    }
-}
-
-impl Printable for BinaryOp {
-    fn print(&self, _shape: Shape, printer: &mut Printer) -> PrintInfo {
-        match self {
-            BinaryOp::EqualsEquals(t) => printer.print_raw_token(t),
-            BinaryOp::NotEquals(t) => printer.print_raw_token(t),
-            BinaryOp::Less(t) => printer.print_raw_token(t),
-            BinaryOp::Greater(t) => printer.print_raw_token(t),
-            BinaryOp::LessEquals(t) => printer.print_raw_token(t),
-            BinaryOp::GreaterEquals(t) => printer.print_raw_token(t),
-            BinaryOp::AndAnd(t) => printer.print_raw_token(t),
-            BinaryOp::OrOr(t) => printer.print_raw_token(t),
-            BinaryOp::And(t) => printer.print_raw_token(t),
-            BinaryOp::Pipe(t) => printer.print_raw_token(t),
-            BinaryOp::Caret(t) => printer.print_raw_token(t),
-            BinaryOp::Instanceof(t) => printer.print_raw_token(t),
-            BinaryOp::LessLess(t) => printer.print_raw_token(t),
-            BinaryOp::GreaterGreater(t) => printer.print_raw_token(t),
-            BinaryOp::Plus(t) => printer.print_raw_token(t),
-            BinaryOp::Minus(t) => printer.print_raw_token(t),
-            BinaryOp::Star(t) => printer.print_raw_token(t),
-            BinaryOp::Slash(t) => printer.print_raw_token(t),
-            BinaryOp::Percent(t) => printer.print_raw_token(t),
-            BinaryOp::Equals(t) => printer.print_raw_token(t),
-            BinaryOp::PlusEquals(t) => printer.print_raw_token(t),
-            BinaryOp::MinusEquals(t) => printer.print_raw_token(t),
-            BinaryOp::StarEquals(t) => printer.print_raw_token(t),
-            BinaryOp::SlashEquals(t) => printer.print_raw_token(t),
-            BinaryOp::PercentEquals(t) => printer.print_raw_token(t),
-            BinaryOp::AndEquals(t) => printer.print_raw_token(t),
-            BinaryOp::PipeEquals(t) => printer.print_raw_token(t),
-            BinaryOp::CaretEquals(t) => printer.print_raw_token(t),
-            BinaryOp::LessLessEquals(t) => printer.print_raw_token(t),
-            BinaryOp::GreaterGreaterEquals(t) => printer.print_raw_token(t),
-        }
-        PrintInfo::default_single_line()
-    }
-    fn leftmost_token(&self) -> TextRange {
-        self.span()
-    }
-    fn rightmost_token(&self) -> TextRange {
-        self.span()
-    }
-}
-
-impl Printable for UnaryOp {
-    fn print(&self, _shape: Shape, printer: &mut Printer) -> PrintInfo {
-        match self {
-            UnaryOp::Not(t) => printer.print_raw_token(t),
-            UnaryOp::Minus(t) => printer.print_raw_token(t),
-        }
-        PrintInfo::default_single_line()
-    }
-    fn leftmost_token(&self) -> TextRange {
-        self.span()
-    }
-    fn rightmost_token(&self) -> TextRange {
-        self.span()
     }
 }
 
