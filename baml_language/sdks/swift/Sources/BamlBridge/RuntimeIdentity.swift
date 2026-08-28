5cafa7fd03be6987fc8a138149317af56df01eaf
Sam Lijin <sam@boundaryml.com>
2026-08-28 16:39:59 -0700

    chore(release): prepare baml_language 0.18.0

--- baml_language/sdks/swift/Sources/BamlBridge/RuntimeIdentity.swift
+++ baml_language/sdks/swift/Sources/BamlBridge/RuntimeIdentity.swift
@@ -1,5 +1,5 @@
 public enum BamlBridgeIdentity {
     public static let runtimeName = "baml-swift"
-    public static let toolchainVersion = "0.17.0"
-    public static let bridgeRuntimeVersion = "0.17.0"
+    public static let toolchainVersion = "0.18.0"
+    public static let bridgeRuntimeVersion = "0.18.0"
 }
